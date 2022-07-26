f14c3811962c6d29860b1424b7ad3ffa3f5288db
vidigummy <vigigummy@gmail.com>
2022-07-26 11:02:30 +0900

    jira webhook check code

--- SRC/HTTP Hook Listeners/jiraWebhookTrigger/jiratrigger1/index.js
+++ SRC/HTTP Hook Listeners/jiraWebhookTrigger/jiratrigger1/index.js
@@ -1,11 +1,14 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    // context.log('JavaScript HTTP trigger function processed a request.');
+
+    context.log(JSON.stringify(req.body));
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 // ddasfdddasfdsa
+
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
