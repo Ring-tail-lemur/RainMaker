9bf732e39d0f3b8b655e455b796a2ea3771ee08b
vidigummy <vigigummy@gmail.com>
2022-07-30 21:41:32 +0900

    promise

--- SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/http-get/http-protocol-module.js
+++ SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/http-get/http-protocol-module.js
@@ -1,17 +1,23 @@
 const request =require('request');
 const axios = require('axios');
 module.exports =  {
-    async httpProtocolCustom(context, options){
-        return request(options, function (err, response, body) {
-            context.log("왜???? 여기 들어오긴 해????");
-            if (err) {
-                context.log(err);
-            } else {
-                context.log("response : " + response);
-                context.log("진짜 왜 그러는거야");
-                return JSON.parse(body);
+    async httpProtocolCustom(context, requestOptions){
+
+
+        return new Promise(function (resolve, reject){
+            let options = {
+                url: requestOptions.url,
+                headers:{
+                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_5_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.webkit'
+                }
             }
-        });
+
+            request(options, function(err,response, body){
+                if(err) reject(err);
+                resolve(JSON.parse(body));
+            });
+
+        })
     }
     // async httpProtocolCustom(context, options){
     //     axios({
