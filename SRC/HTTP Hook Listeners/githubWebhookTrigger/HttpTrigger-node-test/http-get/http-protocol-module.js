189684e7c1c9d009d587efb44a8c20c32a500694
vidigummy <vigigummy@gmail.com>
2022-07-30 21:26:01 +0900

    df

--- SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/http-get/http-protocol-module.js
+++ SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/http-get/http-protocol-module.js
@@ -1,15 +1,27 @@
 const request =require('request');
+const axios = require('axios');
 module.exports =  {
+    // async httpProtocolCustom(context, options){
+    //     await request(options, async function(err,response,body){
+    //       context.log("왜???? 여기 들어오긴 해????");
+    //       if(err){
+    //           context.log(err);
+    //       }else{
+    //           context.log("response : "  + response);
+    //           context.log("진짜 왜 그러는거야");
+    //           return await JSON.parse(body);
+    //       }
+    //     });
+    // }
     async httpProtocolCustom(context, options){
-        await request(options, async function(err,response,body){
-          context.log("왜???? 여기 들어오긴 해????");
-          if(err){
-              context.log(err);
-          }else{
-              context.log("response : "  + response);
-              context.log("진짜 왜 그러는거야");
-              return await JSON.parse(body);
-          }
+        axios({
+            method: 'GET',
+            url: options.uri,
+            headers: options.headers,
+        }).then(function(response){
+            context.log("response : " + response);
+        }).catch(function(err){
+            context.log(error);
         });
     }
 }   
