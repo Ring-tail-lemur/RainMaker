dca7f27f7427f377744a2e105dcb141caa5dc930
vidigummy <vigigummy@gmail.com>
2022-07-30 20:17:26 +0900

    fd

--- SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/http-get/get-commits.js
+++ SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/http-get/get-commits.js
@@ -25,16 +25,15 @@ module.exports ={
         context.log("왜 그러는거야");
         await request(options, function(err,response,body){
           context.log("왜???? 여기 들어오긴 해????");
-        if(err){
-            context.log(err);
-        }else{
-            context.log("response : "  + response);
-            context.log("진짜 왜 그러는거야");
-            return JSON.parse(body);
-        }
+          if(err){
+              context.log(err);
+          }else{
+              context.log("response : "  + response);
+              context.log("진짜 왜 그러는거야");
+              return JSON.parse(body);
+          }
         });
-    
-  }
+    }
 }
 /*
 예시코드... 곧 지울 예정
