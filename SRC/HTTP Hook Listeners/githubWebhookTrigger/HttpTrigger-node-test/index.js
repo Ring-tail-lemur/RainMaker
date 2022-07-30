3ae2c94fed8c241e7022e098dbfa754d09281cb3
vidigummy <vigigummy@gmail.com>
2022-07-30 16:45:39 +0900

    sfda

--- SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/index.js
+++ SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/index.js
@@ -10,7 +10,7 @@ module.exports = async function (context, req) {
     const hookHeaders = req.headers;
 
     // .replace(/['"]+/g, '') <- double quote problem solve (e.g. "\"hi\"")
-    cloudEventObj.hook_event = JSON.stringify(hookHeaders["X-GitHub-Event"]).replace(/['"]+/g, '');
+    cloudEventObj.hook_event = JSON.stringify(hookHeaders["X-GitHub-Event"]);
     cloudEventObj.source = 'github';
 
     // 분기, pull_request || pull_request_review || fork || release || issue_comment || create(branch)
