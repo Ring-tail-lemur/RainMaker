d13ead5c26563d8415a8db0c30a23eeed2b92730
vidigummy <vidigummy@gamil.com>
2022-07-31 22:49:50 +0900

    pull_request가 없었네?

--- SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/issue-comment/issue-comment-module.js
+++ SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/issue-comment/issue-comment-module.js
@@ -4,14 +4,7 @@ async function issueCommentMain(context, hookBody, cloudEventObj){
     cloudEventObj.git_user_remote_id = JSON.stringify(hookBody.sender.id);
     cloudEventObj.event_time = JSON.stringify(hookBody.issue.created_at).replace(/['"]+/g, '');
     cloudEventObj.repository_id = JSON.stringify(hookBody.repository.id);
-
-    const pull_request_url = JSON.stringify(hookBody.issue.pull_request.url).replace(/['"]+/g, '');
     cloudEventObj.isPrivate = JSON.stringify(hookBody.repository.private);
-    if(cloudEventObj.isPrivate == "true"){
-        cloudEventObj.pull_request_remote_id = await issuePullRequest.getPullRequestIdWithToken(context, pull_request_url, '');
-    }else{
-        cloudEventObj.pull_request_remote_id = await issuePullRequest.getPullRequestIdWithoutToken(context, pull_request_url);
-    }
     return cloudEventObj;
 
 }
