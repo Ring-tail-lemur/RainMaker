3123e03418417bba8337a2f18059aee8fb0ea8e4
vidigummy <vidigummy@gamil.com>
2022-07-31 15:11:39 +0900

    dfd

--- SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/http-get/get-commits.js
+++ SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/http-get/get-commits.js
@@ -1,49 +1,37 @@
 const request =require('request');
 
 const getModule = require('./http-protocol-module');
-module.exports ={
-    async getCommitsWithToken(context, uri, gitHubtokenP){
-        context.log("getCommitsWithToken from : " + uri);
-        const githubToken = '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';
-        const options = {
-        uri: uri,
-        headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_5_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.webkit', 'Authorization': 'Bearer ' + githubToken}
-        };
-        request(options, function(err,response,body){
-        if(err){
-            console.log(err);
-        }else{
-            console.log(body);
-        }
-        });
-    },
-}
-module.exports = {
-  async getCommitsAnyToken(context, commits_uri){
-    context.log("getCommitsWithoutToken from : "+ commits_uri);
-    const options = {
-      uri: commits_uri,
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_5_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.webkit'}
-    };
 
-    request(options, function(err,response,body){
-      if(err){
-          context.log(err);
-      }else{
-          const jsoned = JSON.parse(body);
-          const jsonedLength = jsoned.length;
-          for(i = 0; i < jsonedLength; i++){
-              context.log(jsoned[i]);
-          }
-      }
-    });
-    // const commitList =  await getModule.httpProtocolCustom(context, options);
-    // const commitList = await getModule.testHTTP(context);
-    // context.log("commitList : " + JSON.stringify(commitList));
-    // return commitList;
-  },
-} 
+async function getCommitsWithToken(context, uri, gitHubtokenP){
+  context.log("getCommitsWithToken from : " + uri);
+  const githubToken = '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';
+  const options = {
+  uri: uri,
+  headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_5_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.webkit', 'Authorization': 'Bearer ' + githubToken}
+  };
+  request(options, function(err,response,body){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(body);
+    }
+  });
+}
+
+
+async function getCommitsAnyToken(context, commits_uri){
+  context.log("getCommitsWithoutToken from : "+ commits_uri);
+  const options = {
+    uri: commits_uri,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_5_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.webkit'}
+  };
+
+  resultCommitList = await getModule.httpProtocolCustom(context, options);
+}
+
+module.exports.getCommitsWithToken = getCommitsWithToken;
+module.exports.getCommitsWithoutToken = getCommitsAnyToken;
   
 /*
 예시코드... 곧 지울 예정
--- SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/http-get/http-protocol-module.js
+++ SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/http-get/http-protocol-module.js
@@ -1,40 +1,40 @@
 const request =require('request');
 const axios = require('axios');
 
-class httpProtocolModule{
-    constructor(){
-    }
-    async httpProtocolCustom(context, options) {
-        context.log("httpProtocolCustom");
-        axios({
-            method: 'GET',
-            url: options.uri,
-            headers: options.headers,
-        }).then(function(response){
-            context.log("response : " + response);
-        }).catch(function(err){
-            context.log(err);
-        });
-    }
-    async testHTTP(context){
-        const uri = 'https://api.github.com/repos/Ring-tail-lemur/RainMaker/pulls/13/commits';
-        const token = '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';
-        const options = {
-        uri: uri,
-        headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_5_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.webkit', 'Authorization': 'Bearer ' + token}
-        };
-        request(options, function(err,response,body){
-        if(err){
-            context.log(err);
-        }else{
-            const jsoned = JSON.parse(body);
-            const jsonedLength = jsoned.length;
-            for(i = 0; i < jsonedLength; i++){
-                context.log(jsoned[i]);
-            }
+
+async function httpProtocolCustom(context, options) {
+    context.log("httpProtocolCustom");
+    await axios({
+        method: 'GET',
+        url: options.uri,
+        headers: options.headers,
+    }).then(function(response){
+        context.log("response : " + JSON.stringify(response.data));
+        context.log(response.data);
+    }).catch(function(err){
+        context.log(err);
+    });
+}
+async function testHTTP(context){
+    const uri = 'https://api.github.com/repos/Ring-tail-lemur/RainMaker/pulls/13/commits';
+    const token = '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';
+    const options = {
+    uri: uri,
+    headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_5_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.webkit', 'Authorization': 'Bearer ' + token}
+    };
+    request(options, function(err,response,body){
+    if(err){
+        context.log(err);
+    }else{
+        const jsoned = JSON.parse(body);
+        const jsonedLength = jsoned.length;
+        for(i = 0; i < jsonedLength; i++){
+            context.log(jsoned[i]);
         }
-        });
     }
+    });
 }
 
-module.exports = new httpProtocolModule();
+
+module.exports.httpProtocolCustom = httpProtocolCustom;
+module.exports.testHTTP = testHTTP;
