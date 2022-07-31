1f8272d2440418467264b355c921f52c440cfd89
vidigummy <vidigummy@gamil.com>
2022-07-31 18:37:49 +0900

    dd

--- SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/commit/commit-parsing.js
+++ SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/commit/commit-parsing.js
@@ -4,8 +4,8 @@ async function parsingCommit(context, commitObj, parent_pull_request_remote_iden
     const eventHubCommitObj = new Object();
     eventHubCommitObj.source = 'github';
     eventHubCommitObj.parent_pull_request_remote_identifier = parent_pull_request_remote_identifier;
-    eventHubCommitObj.commit_sha = JSON.stringify(commitObj.sha).replace(/['"]+/g, '');
-    eventHubCommitObj.parent_commit_sha = JSON.stringify(commitObj.parents.sha).replace(/['"]+/g, '');
+    eventHubCommitObj.commit_sha = JSON.stringify(commitObj.sha);
+    eventHubCommitObj.parent_commit_sha = JSON.stringify(commitObj.parents.sha);
     eventHubCommitObj.commit_author_name = JSON.stringify(commitObj.commit.author.name);
     eventHubCommitObj.commit_author_email = JSON.stringify(commitObj.commit.author.email);
     eventHubCommitObj.commit_message = JSON.stringify(commitObj.message);
