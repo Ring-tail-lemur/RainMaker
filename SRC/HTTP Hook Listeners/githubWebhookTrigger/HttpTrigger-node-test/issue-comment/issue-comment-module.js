52dabcba5befc5d956db72d217f8fc6fa3f6f85e
vidigummy <vidigummy@gamil.com>
2022-07-31 22:51:58 +0900

    issue 마무리

--- SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/issue-comment/issue-comment-module.js
+++ SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/issue-comment/issue-comment-module.js
@@ -1,6 +1,8 @@
 const issuePullRequest = require('./issue-pull-request.js');
 async function issueCommentMain(context, hookBody, cloudEventObj){
     cloudEventObj.action = JSON.stringify(hookBody.action);
+    cloudEventObj.issue_comment_remote_id = JSON.stringify(hookBody.issue.id);
+    cloudEventObj.issue_comment_repository_number = JSON.stringify(hookBody.issue.number);
     cloudEventObj.git_user_remote_id = JSON.stringify(hookBody.sender.id);
     cloudEventObj.event_time = JSON.stringify(hookBody.issue.created_at).replace(/['"]+/g, '');
     cloudEventObj.repository_id = JSON.stringify(hookBody.repository.id);
