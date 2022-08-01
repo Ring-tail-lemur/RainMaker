6e69de1df2c2483050592c74b7bd2043701ccf30
vidigummy <vigigummy@gmail.com>
2022-08-01 11:19:29 +0900

    pull_request 관련 event

--- SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/pull-request/pull-request-main.js
+++ SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/pull-request/pull-request-main.js
@@ -12,14 +12,20 @@ async function pullRequestMain(context, hookBody, cloudEventObj){
     cloudEventObj.repository_owner_id = JSON.stringify(hookBody.repository.owner.id).replace(/['"]+/g, '');
     cloudEventObj.repository_private = JSON.stringify(hookBody.repository.private).replace(/['"]+/g, '');
     if(cloudEventObj.action == 'opened'){
-        context.log("action : "+ cloudEventObj.action + " || closed event occurred");
+        context.log("action : "+ cloudEventObj.action + " open event occurred");
         resultObj = await open_module.pullRequestOpen(hookBody,cloudEventObj);
     }else if(cloudEventObj.action == 'closed'){
         context.log("action : "+ cloudEventObj.action + " || closed event occurred");
         resultObj = await close_module.pullRequestClose(context, hookBody,cloudEventObj);
     }else{
-        context.log("action : "+ cloudEventObj.action + " || closed event occurred");
-        context.log("not developed yet!");
+        context.log("action : "+ cloudEventObj.action + " event occurred");
+        try{
+            resultObj = await open_module.pullRequestOpen(hookBody,cloudEventObj);
+        }catch(e){
+            context.log(cloudEventObj.action + " event ocurred, but have error");
+            context.log(e);
+        }
+
     }
     return resultObj;
 }
