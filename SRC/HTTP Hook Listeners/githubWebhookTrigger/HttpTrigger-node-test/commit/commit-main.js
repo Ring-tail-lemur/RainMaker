e21aa6293dffe6739d2cfd08e2e856415128d9c8
vidigummy <vidigummy@gamil.com>
2022-08-01 22:45:47 +0900

    df????

--- SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/commit/commit-main.js
+++ SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/commit/commit-main.js
@@ -3,27 +3,34 @@ const sendModule = require('../event-hub/send.js');
 const parsingModule = require('./commit-parsing.js');
 
 async function commitMain(context, commits_url, isPrivate, pull_request_remote_identifier){
-    let commitList = new Object();
     context.log("isPrivate? : " + isPrivate);
 
     if(isPrivate == 'true'){
         //사용자 개인 token 받아오는 logic 필요
         const token = '자신의 토큰을 넣도록 하자. 우리는 척척척 스스로 어린이.';
-        commitList = await getter.getCommitsWithToken(context, commits_url, token,pull_request_remote_identifier);
+        const commitList = await getter.getCommitsWithToken(context, commits_url, token,pull_request_remote_identifier);
         context.log("commitList : " + commitList);
+        commit_cnt = commitList.length;
+        for(let i = 0; i < commit_cnt; i++){
+            const resultObj = await parsingModule.parsingCommit(context,commitList[i],pull_request_remote_remote_remote);
+            context.log(i + "번째 commit : "+ JSON.stringify(resultObj));
+            await sendModule.sender(resultObj,context);
+        }
+        return commit_cnt;
     }else{
         // context.log("commits_url : " + commits_url);
-        commitList = await getter.getCommitsWithoutToken(context, commits_url,pull_request_remote_identifier);
+        const commitList = await getter.getCommitsWithoutToken(context, commits_url,pull_request_remote_identifier);
         context.log("commitList : " + commitList);
+        commit_cnt = commitList.length;
+        for(let i = 0; i < commit_cnt; i++){
+            const resultObj = await parsingModule.parsingCommit(context,commitList[i],pull_request_remote_remote_remote);
+            context.log(i + "번째 commit : "+ JSON.stringify(resultObj));
+            await sendModule.sender(resultObj,context);
+        }
+        return commit_cnt;
     }
     context.log("commitList : " + commitList);
-    commit_cnt = commitList.length;
-    for(let i = 0; i < commit_cnt; i++){
-        const resultObj = await parsingModule.parsingCommit(context,commitList[i],pull_request_remote_remote_remote);
-        context.log(i + "번째 commit : "+ JSON.stringify(resultObj));
-        await sendModule.sender(resultObj,context);
-    }
-    return commit_cnt;
+    
 }
 
 module.exports.commitMain = commitMain;
