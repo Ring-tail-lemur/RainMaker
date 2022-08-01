c6175308969af0c182169833cc37fc222e85f590
vidigummy <vigigummy@gmail.com>
2022-08-01 15:13:30 +0900

    commit head id

--- SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/check-suite/check-suite-module.js
+++ SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/check-suite/check-suite-module.js
@@ -3,12 +3,16 @@ async function checkSuiteMain(context, hookBody, cloudEventObj){
     cloudEventObj.action = JSON.stringify(hookBody.action).replace(/['"]+/g, '');
 
     cloudEventObj.check_suite_id = JSON.stringify(hookBody.check_suite.id);
-    cloudEventObj.status = JSON.stringify(hookBody.check_suite.status);
-    cloudEventObj.conclusion = JSON.stringify(hookBody.check_suite.conclusion);
-    cloudEventObj.head_branch = JSON.stringify(hookBody.check_suite.head_branch);
-    cloudEventObj.head_commit_id = JSON.stringify(hookBody.check_suite.head_commit.id);
-    cloudEventObj.deployment_time = JSON.stringify(hookBody.check_suite.updated_at);
+    cloudEventObj.status = JSON.stringify(hookBody.check_suite.status).replace(/['"]+/g, '');
+    cloudEventObj.conclusion = JSON.stringify(hookBody.check_suite.conclusion).replace(/['"]+/g, '');
+    cloudEventObj.head_branch = JSON.stringify(hookBody.check_suite.head_branch).replace(/['"]+/g, '');
+    cloudEventObj.head_commit_id = JSON.stringify(hookBody.check_suite.head_commit.id).replace(/['"]+/g, '');
+    cloudEventObj.deployment_time = JSON.stringify(hookBody.check_suite.updated_at).replace(/['"]+/g, '');
     
+    const commit_url = JSON.stringify(hookBody.repository.commits_url).replace(/['"]+/g, '').replace('{/sha}','/') + cloudEventObj.head_commit_id;
+    context.log(commit_url);
+    
+
     return cloudEventObj;
 }
 
