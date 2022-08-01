244fabf6d63d6fb29bc93a8f15efcdae4ba40d9d
vidigummy <vidigummy@gamil.com>
2022-08-01 20:54:01 +0900

    test

--- SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/commit/commit-main.js
+++ SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/commit/commit-main.js
@@ -1,15 +1,24 @@
 const getter = require('../http-get/get-commits.js');
-const send_module = require('../event-hub/send.js');
+const sendModule = require('../event-hub/send.js');
+const parsingModule = require('./commit-parsing.js');
 
 async function commitMain(context, commits_url, isPrivate, pull_request_remote_identifier){
+    let commitList = new Object();
     if(isPrivate == 'true'){
         //사용자 개인 token 받아오는 logic 필요
         const token = '자신의 토큰을 넣도록 하자. 우리는 척척척 스스로 어린이.';
-        await getter.getCommitsWithToken(commits_url, token,pull_request_remote_identifier);
+        commitList = await getter.getCommitsWithToken(commits_url, token,pull_request_remote_identifier);
     }else{
         // context.log("commits_url : " + commits_url);
-        await getter.getCommitsWithoutToken(context, commits_url,pull_request_remote_identifier);
+        commitList = await getter.getCommitsWithoutToken(context, commits_url,pull_request_remote_identifier);
     }
+    commit_cnt = commitList.length;
+    for(let i = 0; i < commit_cnt; i++){
+        const resultObj = await parsingModule.parsingCommit(context,commitList[i],pull_request_remote_remote_remote);
+        context.log(i + "번째 commit : "+ JSON.stringify(resultObj));
+        await sendModule.sender(resultObj,context);
+    }
+    return commit_cnt;
 }
 
 module.exports.commitMain = commitMain;
--- SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/index.js
+++ SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/index.js
@@ -22,18 +22,30 @@ module.exports = async function (context, req) {
         context.log('pull_request event occur');
         const resultObj = await pull_request_module.pullRequestMain(context,hookBody,cloudEventObj);
         send_module.sender(resultObj);
+        context.res ={
+            body : JSON.stringify(resultObj)
+        }
     }else if(cloudEventObj.hook_event == 'pull_request_review'){
         context.log("pull_request_review!");
         const resultObj = await pullRequestReviewModule.pullRequestReviewMain(context,hookBody,cloudEventObj);
         send_module.sender(resultObj);
+        context.res ={
+            body : JSON.stringify(resultObj)
+        }
     }else if(cloudEventObj.hook_event == 'pull_request_review_comment'){
         context.log("pull_request_review_comment ocurred");
         const resultObj = await pullRequestReviewCommentModule.pullRequestReviewCommentMain(context,hookBody,cloudEventObj);
         send_module.sender(resultObj);
+        context.res ={
+            body : JSON.stringify(resultObj)
+        }
     }else if(cloudEventObj.hook_event == 'issue_comment'){
         context.log("issue_comment event occurred");
         const resultObj = await issueCommentModule.issueCommentMain(context,hookBody,cloudEventObj);
         send_module.sender(resultObj);
+        context.res ={
+            body : JSON.stringify(resultObj)
+        }
     }else if(cloudEventObj.hook_event == 'check_suite'){
         context.log("check_suite event occurred");
         const resultObj = await checkSuiteModule.checkSuiteMain(context,hookBody,cloudEventObj);
@@ -43,7 +55,7 @@ module.exports = async function (context, req) {
         }
     }else{
         context.res = {
-            body : JSON.stringify(hookHeaders)
+            body : JSON.stringify(cloudEventObj)
         }
     }
 
