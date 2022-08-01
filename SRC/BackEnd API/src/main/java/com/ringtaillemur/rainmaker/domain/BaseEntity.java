cbf1e8ba5ca24a63baaf3d309ada4eaa490a438d
inhyeok <alzpqm@naver.com>
2022-08-01 11:10:41 +0900

    feat: add BaseEntity Anotation

--- SRC/BackEnd API/src/main/java/com/ringtaillemur/rainmaker/domain/BaseEntity.java
+++ SRC/BackEnd API/src/main/java/com/ringtaillemur/rainmaker/domain/BaseEntity.java
@@ -2,16 +2,25 @@ package com.ringtaillemur.rainmaker.domain;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.EntityListeners;
+import javax.persistence.EntityManager;
 import javax.persistence.MappedSuperclass;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 	@CreatedDate
 	private LocalDateTime createTime;
 
 	@LastModifiedDate
 	private LocalDateTime modifiedDate;
+
+
+	@Autowired
+	EntityManager entityManager;
 }
