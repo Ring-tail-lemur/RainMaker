91bdc29349a884ca142a8bc31311b5834fcbdce7
vidigummy <vigigummy@gmail.com>
2022-08-02 17:13:28 +0900

    :bug: double quote problem 해결

--- SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/pull-request/open.js
+++ SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/pull-request/open.js
@@ -1,8 +1,8 @@
 
 async function pullRequestOpen(hookBody, cloudEventObj){
     cloudEventObj.pull_request_opened_number = JSON.stringify(hookBody.number).replace(/['"]]+/g, '');
-    cloudEventObj.pull_request_open_branch = JSON.stringify(hookBody.pull_request.head.ref).replace(/['"]]+/g, '');
-    cloudEventObj.pull_request_open_time = JSON.stringify(hookBody.pull_request.created_at).replace(/['"]]+/g, '');
+    cloudEventObj.pull_request_open_branch = JSON.stringify(hookBody.pull_request.head.ref).replace(/['"]+/g, '');
+    cloudEventObj.pull_request_open_time = JSON.stringify(hookBody.pull_request.created_at).replace(/['"]+/g, '');
     return cloudEventObj;
 }
 
