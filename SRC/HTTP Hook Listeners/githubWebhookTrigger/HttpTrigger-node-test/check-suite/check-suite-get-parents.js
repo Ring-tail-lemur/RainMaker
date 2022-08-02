f3fa6865e15f94d77f5ff914060e06200ba1a8dc
vidigummy <vigigummy@gmail.com>
2022-08-02 11:45:28 +0900

    ????이게 왜 지금까지 안 보였지

--- SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/check-suite/check-suite-get-parents.js
+++ SRC/HTTP Hook Listeners/githubWebhookTrigger/HttpTrigger-node-test/check-suite/check-suite-get-parents.js
@@ -4,7 +4,6 @@ async function checkSuiteGetParentWithToken(context, uri, cloudEventObj, inputTo
     const gitToken = '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';
 
     const options = {
-        uri: uri,
         headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_5_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.webkit', 'Authorization': 'Bearer ' + gitToken}
     };
     const resultObj = await axios({
@@ -20,7 +19,6 @@ async function checkSuiteGetParentWithToken(context, uri, cloudEventObj, inputTo
 
 async function checkSuiteGetParentWithoutToken(context, uri, cloudEventObj){
     const options = {
-        uri: uri,
         headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_5_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.webkit'}
         };
     const resultObj = await axios({
