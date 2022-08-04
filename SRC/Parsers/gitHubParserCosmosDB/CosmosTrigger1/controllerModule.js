b83794909db1783ba25001e174a1066c62019819
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2022-08-04 17:31:50 +0900

    feat : test를 위한  코드 작성

--- SRC/Parsers/gitHubParserCosmosDB/CosmosTrigger1/controllerModule.js
+++ SRC/Parsers/gitHubParserCosmosDB/CosmosTrigger1/controllerModule.js
@@ -1,10 +1,19 @@
 const pullRequestMainModule = require('./pull-request/pullRequestMainModule.js');
+const repositoryMainModule = require('./repository/repositoryMainModule.js');
+
+
+
 async function controllerMain(eventObj, context){
-    if(eventObj.hook_event.hook_event == 'pull_request'){
+
+    const hook_event = eventObj.hook_event;
+
+    if(hook_event == 'pull_request'){
         await pullRequestMainModule.pullRequestMain(eventObj);
-    }else if(eventObj.hook_event == 'commit'){
+    }else if(hook_event == 'commit'){
 
-    }else{
-
+    }else if(hook_event == 'repository'){
+        await repositoryMainModule.repositoryMain(eventObj);
     }
 }
+
+module.exports.controllerMain = controllerMain;
