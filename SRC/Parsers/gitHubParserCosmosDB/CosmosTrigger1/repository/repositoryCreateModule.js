b7b2e9f8213f2c7a7f5dbde31781fdfac8bcd820
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2022-08-04 19:14:44 +0900

    feat: 리포지토리 INSERT 구현 완료

--- SRC/Parsers/gitHubParserCosmosDB/CosmosTrigger1/repository/repositoryCreateModule.js
+++ SRC/Parsers/gitHubParserCosmosDB/CosmosTrigger1/repository/repositoryCreateModule.js
@@ -2,14 +2,50 @@ const pool = require('../ms-sql/msSQLPool');
 
 async function repositoryCreateMain(eventObject, context){
     //repository entity 생성 및 삽입
-    console.log("eventObject : \n", eventObject);
-    const dbpool = await pool;
-    const result = await dbpool.request()
-        .query(`SELECT * FROM dbo.commits where commit_id = ${1}`)
 
-    console.log("result", result);
+    const dbConnectionPool = await pool;
 
-    await dbpool.close();
+    if(eventObject.repository_owner_type == "Organization") {
+        
+        const result = await dbConnectionPool.request()
+            .query(`
+            SELECT git_organization_id 
+            FROM git_organization 
+            WHERE remote_identifier = ${eventObject.repository_owner_id}`
+            );
+        
+        const gitOrganizationId = result.recordset[0].git_organization_id;
+        
+        const repositoryInsertQuery = await dbConnectionPool.request()
+            .query(`
+            INSERT INTO repository (name, owner_type, owner_organization_id)
+            VALUES ('${eventObject.repository_name}', 'ORGANIZATION', ${gitOrganizationId});
+            `);
+        
+        console.log(repositoryInsertQuery);
+        
+    } else if (eventObject.repository_owner_type == "User") {
+        
+        const result = await dbConnectionPool.request()
+            .query(`
+            SELECT git_user_id
+            FROM git_user 
+            WHERE remote_identifier = ${eventObject.repository_owner_id}`
+            );
+        
+        const gitUserId = result.recordset[0].git_user_id;
+
+        const repositoryInsertQuery = await dbConnectionPool.request()
+            .query(`
+            INSERT INTO repository (name, owner_type, owner_user_id)
+            VALUES ('${eventObject.repository_name}', 'USER', ${gitUserId});
+            `);
+
+        console.log(repositoryInsertQuery);
+    }
+    // console.log("eventObject : \n", eventObject);
+
+    await dbConnectionPool.close();
 }
 module.exports.repositoryCreateMain = repositoryCreateMain;
 
@@ -24,4 +60,17 @@ module.exports.repositoryCreateMain = repositoryCreateMain;
     "repository_owner_type":"Organization",
     "repository_owner_id":"107110653"
 }
+
+/*
+레포지토리 생성 이벤트
+{
+    "hook_event":"repository",
+    "source":"github",
+    "action":"created",
+    "repository_remote_id":"520479928",
+    "repository_name":"dfsasdfsf",
+    "repository_owner_type":"Organization",
+    "repository_owner_id":"107110653"
+}
+
 */
