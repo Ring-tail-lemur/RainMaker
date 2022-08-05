f42e520d93034db70e2ac3d8ee4eb0a8e356ab74
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2022-08-05 16:18:09 +0900

    feat; branch make module 파일간 메소드 호출 등록 완료

--- SRC/Parsers/gitHubParserCosmosDB/CosmosTrigger1/controllerModule.js
+++ SRC/Parsers/gitHubParserCosmosDB/CosmosTrigger1/controllerModule.js
@@ -1,10 +1,11 @@
 const pullRequestMainModule = require('./pull-request/pullRequestMainModule.js');
 const repositoryMainModule = require('./repository/repositoryMainModule.js');
-
+const createMainModule = require('./create/createMainModule.js')
 
 
 async function controllerMain(eventObj, context){
 
+    
     const hook_event = eventObj.hook_event;
 
     if(hook_event == 'pull_request'){
@@ -13,6 +14,8 @@ async function controllerMain(eventObj, context){
 
     }else if(hook_event == 'repository'){
         await repositoryMainModule.repositoryMain(eventObj);
+    }else if(hook_event == 'create'){
+        await createMainModule.createMain(eventObj);
     }
 }
 
