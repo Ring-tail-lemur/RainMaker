8af116e1c5369b3be8b4e5f37b79b325326d31c9
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2022-08-05 16:18:09 +0900

    feat; branch make module 파일간 메소드 호출 등록 완료

--- SRC/Parsers/gitHubParserCosmosDB/CosmosTrigger1/create/createMainModule.js
+++ SRC/Parsers/gitHubParserCosmosDB/CosmosTrigger1/create/createMainModule.js
@@ -1,6 +1,10 @@
+const createBranchModule = require('./createBranchModule.js');
 
 async function createMain(eventObject, context) {
     //ref의 type은 branch||tag임, 현재 보고 있는 것은 branch, ref_type이 branch인 것만 처리, tag는 do nothing.
+    if(eventObject.ref_type == 'branch') {
+        await createBranchModule.createBranchMain(eventObject);
+    }
 }
 
 module.exports.createMain = createMain;
