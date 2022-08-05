f01dc9a1a0938bb41386d19ad1ace37b3280e2ae
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2022-08-05 17:52:18 +0900

    :bug fix: <Hook listener> : 배포 문제 버그 hotfix

--- SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/create/create-main-module.js
+++ SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/create/create-main-module.js
@@ -4,9 +4,9 @@ async function createMain(context, hookBody, cloudEventObj){
         cloudEventObj.ref_type = JSON.stringify(hookBody.ref_type).replace(/['"]+/g, '');
         cloudEventObj.branch_name = JSON.stringify(hookBody.ref).replace(/['"]+/g, '');
         cloudEventObj.author_id = JSON.stringify(hookBody.sender.id).replace(/['"]+/g, '');
+        cloudEventObj.repository_id = JSON.stringify(hookBody.repository.id);
         try{
             cloudEventObj.organization_id = JSON.stringify(hookBody.organization.id).replace(/['"]+/g, '');
-            cloudEventObj.repository_id = JSON.stringify(hookBody.repository.id).replace(/['"]+/g, '');
         }catch(e){
             context.log("not a organization repo");
         }
