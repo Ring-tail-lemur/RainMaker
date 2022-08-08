f7e2b67be9591cf190bcb3dba2e8f57debfe2084
vidigummy <vigigummy@gmail.com>
2022-08-08 14:58:34 +0900

    <test> : <JWT> : JWT는 지금부터 지라웹훅트리거다.

--- SRC/HTTP_Hook_Listeners/jiraWebhookTrigger/HttpTrigger1/index.js
+++ SRC/HTTP_Hook_Listeners/jiraWebhookTrigger/HttpTrigger1/index.js
@@ -5,7 +5,7 @@ module.exports = async function (context, req) {
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+    context.log(JSON.stringify(req.body));
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
