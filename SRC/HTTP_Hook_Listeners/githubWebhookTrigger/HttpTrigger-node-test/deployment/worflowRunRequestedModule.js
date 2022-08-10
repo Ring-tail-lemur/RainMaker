4c61e4afdbc7c6cdf6b21b2ad72cf5b41a20c98b
vidigummy <vigigummy@gmail.com>
2022-08-10 10:37:56 +0900

    deploy : listener : workflow_job Module 개발 완료

--- SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/deployment/worflowRunRequestedModule.js
+++
@@ -1,6 +0,0 @@
-async function workflowRunRequestMain(hookBody, cloudEventObj,context){
-    cloudEventObj.event_time = JSON.stringify(hookBody.workflow_run.run_started_at).replace(/['"]+/g, '');
-    return cloudEventObj;
-}
-
-module.exports.workflowRunRequestMain = workflowRunRequestMain;
