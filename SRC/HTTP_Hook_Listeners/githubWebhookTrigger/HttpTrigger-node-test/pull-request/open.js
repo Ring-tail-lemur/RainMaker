6cf152df23cf023bf5537a054e81bc9ac3c388fb
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2022-08-10 14:37:57 +0900

    fix: 보낸 유저 아이디 추가

--- SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/pull-request/open.js
+++ SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/pull-request/open.js
@@ -4,6 +4,7 @@ async function pullRequestOpen(hookBody, cloudEventObj){
     cloudEventObj.pull_request_open_branch = JSON.stringify(hookBody.pull_request.head.ref).replace(/['"]+/g, '');
     cloudEventObj.pull_request_close_branch = JSON.stringify(hookBody.pull_request.base.ref).replace(/['"]+/g, '');
     cloudEventObj.pull_request_open_time = JSON.stringify(hookBody.pull_request.created_at).replace(/['"]+/g, '');
+    cloudEventObj.pull_request_user_id = JSON.stringify(hookBody.pull_request.user.id).replace(/['"]+/g, '');
     return cloudEventObj;
 }
 
