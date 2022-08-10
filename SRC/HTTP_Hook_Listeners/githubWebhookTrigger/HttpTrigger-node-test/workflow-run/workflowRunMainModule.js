f3d2a22024b7653e5f4ec0d21bd4a7a0dc400f13
vidigummy <vigigummy@gmail.com>
2022-08-10 17:14:09 +0900

    hotfix

--- SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/workflow-run/workflowRunMainModule.js
+++ SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/workflow-run/workflowRunMainModule.js
@@ -3,10 +3,10 @@ const completeModule = require('./workflowRunCompletedModule.js');
 async function workflowRunMain(hookBody, cloudEventObj, context){
     cloudEventObj.action = JSON.stringify(hookBody.action).replace(/['"]+/g, '');
     cloudEventObj.workflow_run_remote_id = JSON.stringify(hookBody.workflow_run.id).replace(/['"]+/g, '');
-    cloudEventObj.workflow_remote_id = JSON.stringify(hookBody.workflow.workflow_id);
-    cloudEventObj.workflow_head_branch = JSON.stringify(hookBody.workflow.head_branch).replace(/['"]+/g, '');
-    cloudEventObj.workflow_run_number = JSON.stringify(hookBody.workflow.run_number);
-    cloudEventObj.workflow_check_suite_id = JSON.stringify(hookBody.workflow.check_suite_id).replace(/['"]+/g, '');
+    cloudEventObj.workflow_remote_id = JSON.stringify(hookBody.workflow.id).replace(/['"]+/g, '');
+    cloudEventObj.workflow_head_branch = JSON.stringify(hookBody.workflow_run.head_branch).replace(/['"]+/g, '');
+    cloudEventObj.workflow_run_number = JSON.stringify(hookBody.workflow_run.run_number).replace(/['"]+/g, '');
+    cloudEventObj.workflow_check_suite_id = JSON.stringify(hookBody.workflow_run.check_suite_id).replace(/['"]+/g, '');
     cloudEventObj.repository_id = JSON.stringify(hookBody.repository.id).replace(/['"]+/g, '');
     cloudEventObj.repository_owner_type = JSON.stringify(hookBody.repository.owner.type).replace(/['"]+/g, '');
     cloudEventObj.repository_owner_id = JSON.stringify(hookBody.repository.owner.id).replace(/['"]+/g, '');
