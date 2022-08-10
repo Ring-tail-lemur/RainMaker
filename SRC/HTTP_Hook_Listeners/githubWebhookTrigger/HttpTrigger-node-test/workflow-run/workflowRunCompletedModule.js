71a9ae25d6df2c6927756a989b962a7ee00a3654
vidigummy <vigigummy@gmail.com>
2022-08-10 14:51:29 +0900

    hotfix

--- SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/workflow-run/workflowRunCompletedModule.js
+++
@@ -1,7 +0,0 @@
-async function workflowRunCompleteMain(hookBody, cloudEventObj, context){
-    cloudEventObj.conclusion = JSON.stringify(hookBody.workflow_run.conclusion).replace(/['"]+/g, '');
-    cloudEventObj.event_time = JSON.stringify(hookBody.workflow_run.updated_at).replace(/['"]+/g, '');
-    return cloudEventObj;
-}
-
-module.exports.workflowRunCompleteMain = workflowRunCompleteMain;
