d9271b50632efe3f82e137e826f9fc6ba7f85041
vidigummy <vidigummy@gamil.com>
2022-08-11 19:47:45 +0900

    dev : listener : branchModule 수정중

--- SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/create/create-main-module.js
+++ SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/create/create-main-module.js
@@ -1,20 +1,8 @@
 async function createMain(context, hookBody, cloudEventObj){
-    try{
-        cloudEventObj.action = 'create_ref';
-        cloudEventObj.ref_type = JSON.stringify(hookBody.ref_type).replace(/['"]+/g, '');
-        cloudEventObj.branch_name = JSON.stringify(hookBody.ref).replace(/['"]+/g, '');
-        cloudEventObj.author_id = JSON.stringify(hookBody.sender.id).replace(/['"]+/g, '');
-        cloudEventObj.repository_id = JSON.stringify(hookBody.repository.id).replace(/['"]+/g, '');
-        cloudEventObj.repository_full_name = JSON.stringify(hookBody.repository.full_name).replace(/['"]+/g, '');
-        try{
-            cloudEventObj.organization_id = JSON.stringify(hookBody.organization.id).replace(/['"]+/g, '');
-        }catch(e){
-            context.log("not a organization repo");
-        }
-        return cloudEventObj;
-    }catch(e){
-        cloudEventObj.action = 'tag';
-        return cloudEventObj;
+    cloudEventObj.ref_type = JSON.stringify(hookBody.action).replace(/['"]+/g, '');
+    if(cloudEventObj.ref_type == 'branch'){
+        //branch 생성 이벤트
+        
     }
     
 
