e06bbc5b691230ae5824efeb582725fe20c7ac71
vidigummy <vigigummy@gmail.com>
2022-08-12 15:17:13 +0900

    fix : listener : createModule event time 추가

--- SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/create/create-main-module.js
+++ SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/create/create-main-module.js
@@ -1,7 +1,9 @@
 const branchModule = require('./branchModule.js');
 const tagModule = require('./tagModule.js');
+const timeModule = require('../utils/getCurrentTimeModule.js');
 async function createMain(context, hookBody, cloudEventObj){
     cloudEventObj.ref_type = JSON.stringify(hookBody.ref_type).replace(/['"]+/g, '');
+    cloudEventObj.event_time = await timeModule.getCurrentTime();
     if(cloudEventObj.ref_type == 'branch'){
         cloudEventObj.action = 'created_branch';
         return await branchModule.branchMain(hookBody, cloudEventObj, context);
