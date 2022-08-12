2e655b4f81703129cebbf3ced5471016fb5ff006
vidigummy <vigigummy@gmail.com>
2022-08-12 15:12:23 +0900

    hotfix : listener : 왜...? 자꾸... 까먹어..?

--- SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/create/tagModule.js
+++ SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/create/tagModule.js
@@ -2,7 +2,7 @@ async function tagMain(hookBody, cloudEventObj, context){
     cloudEventObj.ref_name = JSON.stringify(hookBody.ref).replace(/['"]+/g, '');
     cloudEventObj.owner_id = JSON.stringify(hookBody.repository.owner.id).replace(/['"]+/g, '');
     cloudEventObj.repository_id = JSON.stringify(hookBody.repository.id).replace(/['"]+/g, '');
-    cloudEventObj.owner_type = JSON.stringify(hookBody.owner.type).replace(/['"]+/g, '');
+    cloudEventObj.owner_type = JSON.stringify(hookBody.repository.owner.type).replace(/['"]+/g, '');
     cloudEventObj.repository_private = JSON.stringify(hookBody.repository.private).replace(/['"]+/g, '');
     return cloudEventObj;
 }
