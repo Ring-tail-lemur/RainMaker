ea1d9e8b982442ee7c464580ec251c32641b3476
vidigummy <vigigummy@gmail.com>
2022-08-12 13:24:49 +0900

    add : listener : tag 관련 모듈 제작 완료

---
+++ SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/create/tagModule.js
@@ -0,0 +1,10 @@
+async function tagMain(hookBody, cloudEventObj, context){
+    cloudEventObj.ref_name = JSON.stringify(hookBody.ref).replace(/['"]+/g, '');
+    cloudEventObj.owner_id = JSON.stringify(hookBody.repository.owner.id).replace(/['"]+/g, '');
+    cloudEventObj.repository_id = JSON.stringify(hookBody.repository.id).replace(/['"]+/g, '');
+    cloudEventObj.owner_type = JSON.stringify(hookBody.owner.type).replace(/['"]+/g, '');
+    cloudEventObj.repository_private = JSON.stringify(hookBody.repository.private).replace(/['"]+/g, '');
+    return cloudEventObj;
+}
+
+module.exports.tagMain = tagMain;
