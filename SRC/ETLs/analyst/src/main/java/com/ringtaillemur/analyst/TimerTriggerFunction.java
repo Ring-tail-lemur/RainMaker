7e3f7abfa22aa033585eea8dab4357583e87ccb1
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-08-12 16:19:44 +0900

    GitHub action etl (#203)
    * add : action : analyst deploy
    
    * ??
    
    * deployment test 1
    
    * hotfix
    
    * df
    
    * sd
    
    * test3
    
    * hmm
    
    * test1
    
    * test1
    
    * etl deploy는 되어야지...
    
    Co-authored-by: vidigummy <vidigummy@gamil.com>
    Co-authored-by: vidigummy <vigigummy@gmail.com>

--- SRC/ETLs/analyst/src/main/java/com/ringtaillemur/analyst/TimerTriggerFunction.java
+++ SRC/ETLs/analyst/src/main/java/com/ringtaillemur/analyst/TimerTriggerFunction.java
@@ -1,22 +1,25 @@
 package com.ringtaillemur.analyst;
 
-import java.io.IOException;
-
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.TimerTrigger;
 import com.ringtaillemur.analyst.analysislogic.dorametric.LeadTimeForChange;
+import java.io.IOException;
 
+// test1
 public class TimerTriggerFunction {
 
+  LeadTimeForChange leadTimeForChange = LeadTimeForChange.getLeadTimeForChange();
 
-	LeadTimeForChange leadTimeForChange = LeadTimeForChange.getLeadTimeForChange();
-
-	@FunctionName("TimerTrigger-Java")
-	public void run(
-		@TimerTrigger(name = "timerInfo", schedule = "0 */10 * * * *") String timerInfo,
-		final ExecutionContext context
-	) throws IOException {
-		leadTimeForChange.calculateLeadTimeForChange();
-	}
+  @FunctionName("TimerTrigger-Java")
+  public void run(
+    @TimerTrigger(
+      name = "timerInfo",
+      schedule = "0 */10 * * * *"
+    ) String timerInfo,
+    final ExecutionContext context
+  )
+    throws IOException {
+    leadTimeForChange.calculateLeadTimeForChange();
+  }
 }
