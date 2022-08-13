a348db2dd838b3213658dcf6948a5f3383a1f886
vidigummy <vidigummy@gamil.com>
2022-08-13 19:36:30 +0900

    add : listener : pr_url 가지고 관련 pr 아이디 가져오기

--- SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/http-get/http-protocol-module.js
+++ SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/http-get/http-protocol-module.js
@@ -1,19 +1,13 @@
-const request =require('request');
 const axios = require('axios');
-const parsingModule = require('../commit/commit-parsing.js');
 
 async function httpProtocolCustom(context, options) {
     // context.log(options);
-    const commitsList = await axios({
+    const httpResult = await axios({
         method: 'GET',
         url: options.uri,
         headers: options.headers,
     });
-    // context.log("--------- httpModule ---------\n"+JSON.stringify(commitsList.data)+"\n--------- httpModule ---------\n");
-    return commitsList.data;
+    return httpResult.data;
 }
 
-
-
-// httpProtocolCustom('',{uri: 'https://api.github.com/repos/Ring-tail-lemur/test-for-fake-project/pulls/170/commits',headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_5_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.webkit'}});
 module.exports.httpProtocolCustom = httpProtocolCustom;
