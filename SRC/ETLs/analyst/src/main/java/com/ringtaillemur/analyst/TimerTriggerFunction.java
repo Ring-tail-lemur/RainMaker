713f26fe9e65379004d822157e3534770befee7f
vidigummy <vidigummy@gamil.com>
2022-08-13 18:35:47 +0900

    test7

--- SRC/ETLs/analyst/src/main/java/com/ringtaillemur/analyst/TimerTriggerFunction.java
+++ SRC/ETLs/analyst/src/main/java/com/ringtaillemur/analyst/TimerTriggerFunction.java
@@ -6,7 +6,7 @@ import com.microsoft.azure.functions.annotation.TimerTrigger;
 import com.ringtaillemur.analyst.analysislogic.dorametric.LeadTimeForChange;
 import java.io.IOException;
 
-// test6
+// test7
 public class TimerTriggerFunction {
 
   LeadTimeForChange leadTimeForChange = LeadTimeForChange.getLeadTimeForChange();
