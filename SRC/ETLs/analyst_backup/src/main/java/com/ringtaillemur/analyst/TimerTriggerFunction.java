227475ca6d1743882dd1894376de118274ef3f2b
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-08-13 18:47:29 +0900

    GitHub action etl (#205)
    * add : action : analyst deploy
    
    * ??
    
    * deployment test 1
    
    * hotfix
    
    * df
    
    * sd
    
    * test3
    
    * hmm
    
    * test1
    
    * test1
    
    * etl deploy는 되어야지...
    
    * etl pipeline test 2
    
    * test3
    
    * action test2
    
    * ?
    
    * test3
    
    * ...
    
    * etl 배포는 하고! 좀!
    
    * test4
    
    * ?왜 메인이 아니야
    
    * 테스트5
    
    * ?
    
    * test6
    
    * ?
    
    * test7
    
    * dd
    
    * test8
    
    Co-authored-by: vidigummy <vidigummy@gamil.com>
    Co-authored-by: vidigummy <vigigummy@gmail.com>

--- SRC/ETLs/analyst_backup/src/main/java/com/ringtaillemur/analyst/TimerTriggerFunction.java
+++ SRC/ETLs/analyst_backup/src/main/java/com/ringtaillemur/analyst/TimerTriggerFunction.java
@@ -6,7 +6,7 @@ import com.microsoft.azure.functions.annotation.TimerTrigger;
 import com.ringtaillemur.analyst.analysislogic.dorametric.LeadTimeForChange;
 import java.io.IOException;
 
-// test8
+// test3
 public class TimerTriggerFunction {
 
   LeadTimeForChange leadTimeForChange = LeadTimeForChange.getLeadTimeForChange();
