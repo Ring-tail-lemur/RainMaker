5a231335bf6be73567710e405b578df238108fda
vidigummy <vigigummy@gmail.com>
2022-08-15 15:48:36 +0900

    test 14

--- SRC/BackEnd_API/src/test/java/com/ringtaillemur/rainmaker/RainmakerWebserverApplicationTest.java
+++ SRC/BackEnd_API/src/test/java/com/ringtaillemur/rainmaker/RainmakerWebserverApplicationTest.java
@@ -21,7 +21,7 @@ class RainmakerWebserverApplicationTest {
     List<GitUser> all = gitUserRepository.findAll();
     for (GitUser gitUser : all) {
       System.out.println("gitUser = " + gitUser);
-      //13
+      //14
     }
   }
 }
