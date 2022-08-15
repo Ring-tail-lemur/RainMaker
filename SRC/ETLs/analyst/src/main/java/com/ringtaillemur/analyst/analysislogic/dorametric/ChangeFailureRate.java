7f4cf49e6208d925a20ce7355b2be2d0a7827367
inhyeok <alzpqm@naver.com>
2022-08-15 16:49:32 +0900

    feat: DORA ETL 완료

--- SRC/ETLs/analyst/src/main/java/com/ringtaillemur/analyst/analysislogic/dorametric/ChangeFailureRate.java
+++ SRC/ETLs/analyst/src/main/java/com/ringtaillemur/analyst/analysislogic/dorametric/ChangeFailureRate.java
@@ -1,5 +1,21 @@
 package com.ringtaillemur.analyst.analysislogic.dorametric;
 
+import com.ringtaillemur.analyst.query.OlapQuery;
+import com.ringtaillemur.analyst.query.QueryRunner;
+
 public class ChangeFailureRate {
 
+	private static final ChangeFailureRate changeFailureRate = new ChangeFailureRate();
+	private QueryRunner queryRunner = QueryRunner.getQueryRunner();
+
+	private ChangeFailureRate() {
+	}
+
+	public static ChangeFailureRate getChangeFailureRate() {
+		return changeFailureRate;
+	}
+
+	public void calculateChangeFailureRate() {
+		queryRunner.runQuery(OlapQuery.MAKE_CHANGE_FAILURE_RATE);
+	}
 }
