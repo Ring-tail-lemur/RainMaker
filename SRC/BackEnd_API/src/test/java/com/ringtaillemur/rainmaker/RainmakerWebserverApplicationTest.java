bce706a7c6365dc12c3878043226bca253ac5490
vidigummy <vigigummy@gmail.com>
2022-08-15 11:42:25 +0900

    test1

--- SRC/BackEnd_API/src/test/java/com/ringtaillemur/rainmaker/RainmakerWebserverApplicationTest.java
+++ SRC/BackEnd_API/src/test/java/com/ringtaillemur/rainmaker/RainmakerWebserverApplicationTest.java
@@ -2,28 +2,26 @@ package com.ringtaillemur.rainmaker;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.ringtaillemur.rainmaker.domain.GitOrganization;
+import com.ringtaillemur.rainmaker.domain.GitUser;
+import com.ringtaillemur.rainmaker.repository.GitUserRepository;
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.ringtaillemur.rainmaker.domain.GitOrganization;
-import com.ringtaillemur.rainmaker.domain.GitUser;
-import com.ringtaillemur.rainmaker.repository.GitUserRepository;
-
 @SpringBootTest
 class RainmakerWebserverApplicationTest {
 
-	@Autowired
-	GitUserRepository gitUserRepository;
+  @Autowired
+  GitUserRepository gitUserRepository;
 
-	@Test
-	void test(){
-		List<GitUser> all = gitUserRepository.findAll();
-		for (GitUser gitUser : all) {
-			System.out.println("gitUser = " + gitUser);
-		}
-	}
-
+  @Test
+  void test() {
+    List<GitUser> all = gitUserRepository.findAll();
+    for (GitUser gitUser : all) {
+      System.out.println("gitUser = " + gitUser);
+      //
+    }
+  }
 }
