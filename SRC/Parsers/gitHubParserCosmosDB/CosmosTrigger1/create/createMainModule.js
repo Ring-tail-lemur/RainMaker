182fd70f883da5e3dbba25b3fcfab87579c294d8
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2022-08-17 17:17:35 +0900

    feat: tag insert module

--- SRC/Parsers/gitHubParserCosmosDB/CosmosTrigger1/create/createMainModule.js
+++ SRC/Parsers/gitHubParserCosmosDB/CosmosTrigger1/create/createMainModule.js
@@ -5,8 +5,8 @@ async function createMain(pool, eventObject, context) {
     //ref의 type은 branch||tag임, 현재 보고 있는 것은 branch, ref_type이 branch인 것만 처리, tag는 do nothing.
     if(eventObject.ref_type == 'branch') {
         await createBranchModule.createBranchMain(pool, eventObject, context);
-    } else if(eventObject.ref_type == 'created_tag') {
-        await createTagModule.createTagMain();
+    } else if(eventObject.ref_type == 'tag') {
+        await createTagModule.createTagMain(pool, eventObject);
     }
 }
 
--- SRC/Parsers/gitHubParserCosmosDB/CosmosTrigger1/create/tag/createTagRepository.js
+++ SRC/Parsers/gitHubParserCosmosDB/CosmosTrigger1/create/tag/createTagRepository.js
@@ -1,4 +1,31 @@
-async function insertTagByRepoId() {
+async function insertTagByRepoIdAndUserId(dbConnectionPool, tag_name, repository_remote_id, author_id){
+
+    // const dbConnectionPool = await pool;
+
+    const sqlQuery = `
+    INSERT INTO branch (name, repository_id, git_user_id, create_type)
+    VALUES ( '${tag_name}', 
+    (
+        SELECT repository_id
+        FROM repository
+        WHERE remote_identifier = ${repository_remote_id}
+    ),
+    (
+        SELECT git_user_id
+        FROM git_user
+        WHERE remote_identifier = ${author_id}
+    ), 'TAG');
+    `;
+    console.log(sqlQuery);
+
+    try {
+        await dbConnectionPool.request()
+            .query(sqlQuery);
+    } catch (e) {
+        console.log(e);
+    }
 
 }
+
+module.exports.insertTagByRepoIdAndUserId = insertTagByRepoIdAndUserId;
 
