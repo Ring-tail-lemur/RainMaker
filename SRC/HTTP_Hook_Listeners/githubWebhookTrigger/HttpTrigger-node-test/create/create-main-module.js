36b87b9e71671d71de822440343d04053b4faf31
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-08-18 19:01:06 +0900

    refact : listener : create/delete Module

--- SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/create/create-main-module.js
+++ SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/create/create-main-module.js
@@ -1,18 +1,15 @@
-const branchModule = require('./branchModule.js');
-const tagModule = require('./tagModule.js');
-const timeModule = require('../utils/getCurrentTimeModule.js');
 async function createMain(context, hookBody, cloudEventObj){
+    cloudEventObj.ref = JSON.stringify(hookBody.ref).replace(/['"]+/g, '');
     cloudEventObj.ref_type = JSON.stringify(hookBody.ref_type).replace(/['"]+/g, '');
-    cloudEventObj.event_time = await timeModule.getCurrentTime();
-    if(cloudEventObj.ref_type == 'branch'){
-        cloudEventObj.action = 'created_branch';
-        return await branchModule.branchMain(hookBody, cloudEventObj, context);
-    }else if(cloudEventObj.ref_type == 'tag'){
-        cloudEventObj.action = 'created_tag';
-        return await tagModule.tagMain(hookBody, cloudEventObj, context);
-    }else{
-        return new Object();
-    }
+    cloudEventObj.action = (ref_type == 'branch'? 'create_branch' : 'create_tag');
+    cloudEventObj.master_branch = JSON.stringify(hookBody.master_branch).replace(/['"]+/g, '');
+    cloudEventObj.description = JSON.stringify(hookBody.description).replace(/['"]+/g, '');
+    cloudEventObj.pusher_type = JSON.stringify(hookBody.pusher_type).replace(/['"]+/g, '');
+    cloudEventObj.repository = JSON.stringify(hookBody.repository.url).replace(/['"]+/g, '');
+    cloudEventObj.organization = JSON.stringify(hookBody.organization.url).replace(/['"]+/g, '');
+    cloudEventObj.sender = JSON.stringify(hookBody.sender.url).replace(/['"]+/g, '');
+
+    return cloudEventObj;
 }
 
 module.exports.createMain = createMain;
