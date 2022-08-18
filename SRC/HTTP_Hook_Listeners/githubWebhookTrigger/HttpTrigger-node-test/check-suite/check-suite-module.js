3cb5ef9a0c0c847444b06983fd68210faaccdceb
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-08-18 18:47:00 +0900

    refact : listener : check-suite-module

--- SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/check-suite/check-suite-module.js
+++ SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/check-suite/check-suite-module.js
@@ -1,25 +1,10 @@
-const checkSuiteParents = require('./check-suite-get-parents');
-
 async function checkSuiteMain(context, hookBody, cloudEventObj){
     cloudEventObj.action = JSON.stringify(hookBody.action).replace(/['"]+/g, '');
-
-    cloudEventObj.check_suite_id = JSON.stringify(hookBody.check_suite.id);
-    cloudEventObj.status = JSON.stringify(hookBody.check_suite.status).replace(/['"]+/g, '');
-    cloudEventObj.conclusion = JSON.stringify(hookBody.check_suite.conclusion).replace(/['"]+/g, '');
-    cloudEventObj.head_branch = JSON.stringify(hookBody.check_suite.head_branch).replace(/['"]+/g, '');
-    cloudEventObj.head_commit_id = JSON.stringify(hookBody.check_suite.head_commit.id).replace(/['"]+/g, '');
-    cloudEventObj.deployment_time = JSON.stringify(hookBody.check_suite.updated_at).replace(/['"]+/g, '');
-    const isPrivateRepo = JSON.stringify(hookBody.repository.private).replace(/['"]+/g, '');
-    const commit_url = JSON.stringify(hookBody.repository.commits_url).replace(/['"]+/g, '').replace('{/sha}','/') + cloudEventObj.head_commit_id;
-    // context.log(commit_url);
-
-    if(isPrivateRepo == 'true'){
-        return(await checkSuiteParents.checkSuiteGetParentWithToken(context,commit_url,cloudEventObj, 'private_token')); 
-       
-    }else{
-        return (await checkSuiteParents.checkSuiteGetParentWithoutToken(context, commit_url, cloudEventObj));
-    }
-
+    cloudEventObj.check_suite = JSON.stringify(hookBody.check_suite.url).replace(/['"]+/g, '');
+    cloudEventObj.repository = JSON.stringify(hookBody.repository.url).replace(/['"]+/g, '');
+    cloudEventObj.organization = JSON.stringify(hookBody.organization.url).replace(/['"]+/g, '');
+    cloudEventObj.sender = JSON.stringify(hookBody.sender.url).replace(/['"]+/g, '');
+    return cloudEventObj;
 }
 
 module.exports.checkSuiteMain = checkSuiteMain;
