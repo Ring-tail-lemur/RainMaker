fb09d89ce6291cbcb1fd4841e512cc297ed25277
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2022-08-22 16:23:46 +0900

    fix: pull_request db change

--- SRC/ms-sql-querys/create-table-query-mssql.sql
+++ SRC/ms-sql-querys/create-table-query-mssql.sql
@@ -30,8 +30,8 @@ CREATE TABLE pull_request
     pull_request_id                  BIGINT    NOT NULL PRIMARY KEY, --remote_identifier
     pull_request_number              BIGINT    NOT NULL,
     repository_id                    BIGINT    NOT NULL,
-    pull_request_open_branch_id      BIGINT    NOT NULL,
-    pull_request_close_branch_id     BIGINT    NOT NULL,
+    pull_request_open_branch_id      VARCHAR(255)    NOT NULL,
+    pull_request_close_branch_id     VARCHAR(255)    NOT NULL,
     created_date                     DATETIME2 NOT NULL DEFAULT GETDATE(),
     modified_date                    DATETIME2 NOT NULL DEFAULT GETDATE(),
     lead_time_for_change_process_end BIT       NOT NULL DEFAULT 0
