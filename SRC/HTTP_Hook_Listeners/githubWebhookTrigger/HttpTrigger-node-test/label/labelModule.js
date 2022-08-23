2e9bda145c421e88250cee2f07ecc00eadc77305
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2022-08-23 11:57:33 +0900

    fix: label 잘못 넣은거 픽스

--- SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/label/labelModule.js
+++ SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/label/labelModule.js
@@ -3,9 +3,9 @@ async function labelMain(hookBody, cloudEventObj, context){
     cloudEventObj.label = JSON.stringify(hookBody.label.name).replace(/['"]+/g, '');
     cloudEventObj.issue_label_id = JSON.stringify(hookBody.label.id).replace(/['"]+/g, '');
     cloudEventObj.repository_id = JSON.stringify(hookBody.repository.id).replace(/['"]+/g, '');
-    if(cloudEventObj.ref_type == 'created'){
+    if(cloudEventObj.action == 'created'){
         cloudEventObj.action = 'create_label';
-    }else if(cloudEventObj.ref_type == 'deleted'){
+    }else if(cloudEventObj.action == 'deleted'){
         cloudEventObj.action = 'delete_label';
     }else{
         cloudEventObj.hookBody = 'notYet';
