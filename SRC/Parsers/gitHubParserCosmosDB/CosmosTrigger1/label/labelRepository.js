e9ca8dcbb1c7a93c5b0aa89029c773a41f4e35da
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2022-08-23 12:30:45 +0900

    feat: label db insert and delete logic ad

--- SRC/Parsers/gitHubParserCosmosDB/CosmosTrigger1/label/labelRepository.js
+++ SRC/Parsers/gitHubParserCosmosDB/CosmosTrigger1/label/labelRepository.js
@@ -1,8 +1,22 @@
-async function insertIssueLabel(dbConnectionPool, releaseId, labelId, labelName, repositoryId, context){
+async function insertIssueLabel(dbConnectionPool, labelId, labelName, repositoryId, context){
 
     const sqlQuery = `
-    INSERT INTO issue_label([label], release_id, issue_label_id, repository_id)
-    VALUES ('${labelName}', ${releaseId}, ${labelId}, ${repositoryId})
+    INSERT INTO issue_label([label], issue_label_id, repository_id)
+    VALUES ('${labelName}', ${labelId}, ${repositoryId})
+    `;
+    console.log(sqlQuery);
+
+    await dbConnectionPool.request()
+        .query(sqlQuery);
+
+}
+
+async function deleteIssueLabel(dbConnectionPool, labelId, repositoryId, context){
+
+    const sqlQuery = `
+    DELETE FROM issue_label
+    WHERE issue_label_id = ${labelId}
+    AND repository_id = ${repositoryId}
     `;
     console.log(sqlQuery);
 
@@ -12,3 +26,4 @@ async function insertIssueLabel(dbConnectionPool, releaseId, labelId, labelName,
 }
 
 module.exports.insertIssueLabel = insertIssueLabel;
+module.exports.deleteIssueLabel = deleteIssueLabel;
