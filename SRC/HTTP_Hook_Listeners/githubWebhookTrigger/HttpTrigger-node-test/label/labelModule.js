18df2ea870e624e16015947fa342aacb88ee54dd
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2022-08-23 12:05:07 +0900

    fix: label action 추가

--- SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/label/labelModule.js
+++ SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/label/labelModule.js
@@ -3,15 +3,9 @@ async function labelMain(hookBody, cloudEventObj, context){
     cloudEventObj.label = JSON.stringify(hookBody.label.name).replace(/['"]+/g, '');
     cloudEventObj.issue_label_id = JSON.stringify(hookBody.label.id).replace(/['"]+/g, '');
     cloudEventObj.repository_id = JSON.stringify(hookBody.repository.id).replace(/['"]+/g, '');
-    if(cloudEventObj.action == 'created'){
-        cloudEventObj.action = 'create_label';
-    }else if(cloudEventObj.action == 'deleted'){
-        cloudEventObj.action = 'delete_label';
-    }else{
-        cloudEventObj.hookBody = 'notYet';
-    }
-    return cloudEventObj;
+    cloudEventObj.action = JSON.stringify(hookBody.action).replace(/['"]+/g, '');
 
+    return cloudEventObj;
 }
 
 module.exports.labelMain = labelMain;
