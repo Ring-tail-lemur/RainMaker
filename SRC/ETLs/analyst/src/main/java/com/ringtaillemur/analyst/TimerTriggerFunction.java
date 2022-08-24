2cd49961aa4cdf6d6a9ee0172c1f2f3badd04118
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-08-24 15:05:41 +0900

    hotfix test : action : 배포 실패 error

--- SRC/ETLs/analyst/src/main/java/com/ringtaillemur/analyst/TimerTriggerFunction.java
+++ SRC/ETLs/analyst/src/main/java/com/ringtaillemur/analyst/TimerTriggerFunction.java
@@ -1,13 +1,5 @@
 package com.ringtaillemur.analyst;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.TimerTrigger;
@@ -17,22 +9,33 @@ import com.ringtaillemur.analyst.analysislogic.dorametric.TimeToRestoreService;
 import com.ringtaillemur.analyst.analysislogic.dorametric.UpdateCommitsReleaseId;
 import com.ringtaillemur.analyst.dto.ReleaseDto;
 import com.ringtaillemur.analyst.restapi.GetCommitsCompare;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-// test8
+// test what?
 public class TimerTriggerFunction {
 
-	LeadTimeForChange leadTimeForChange = LeadTimeForChange.getLeadTimeForChange();
-	ChangeFailureRate changeFailureRate = ChangeFailureRate.getChangeFailureRate();
-	TimeToRestoreService timeToRestoreService = TimeToRestoreService.getTimeToRestoreService();
-	UpdateCommitsReleaseId updateCommitsReleaseId = UpdateCommitsReleaseId.getUpdateCommitsReleaseId();
+  LeadTimeForChange leadTimeForChange = LeadTimeForChange.getLeadTimeForChange();
+  ChangeFailureRate changeFailureRate = ChangeFailureRate.getChangeFailureRate();
+  TimeToRestoreService timeToRestoreService = TimeToRestoreService.getTimeToRestoreService();
+  UpdateCommitsReleaseId updateCommitsReleaseId = UpdateCommitsReleaseId.getUpdateCommitsReleaseId();
 
-	@FunctionName("TimerTrigger-Java")
-	public void run(
-		@TimerTrigger(name = "timerInfo", schedule = "1-50 * * * * *") String timerInfo,
-		final ExecutionContext context) throws Exception {
-		updateCommitsReleaseId.calculateUpdateCommitsReleaseId();
-		leadTimeForChange.calculateLeadTimeForChange();
-		changeFailureRate.calculateChangeFailureRate();
-		timeToRestoreService.calculateTimeToRestoreService();
-	}
+  @FunctionName("TimerTrigger-Java")
+  public void run(
+    @TimerTrigger(
+      name = "timerInfo",
+      schedule = "1-50 * * * * *"
+    ) String timerInfo,
+    final ExecutionContext context
+  )
+    throws Exception {
+    updateCommitsReleaseId.calculateUpdateCommitsReleaseId();
+    leadTimeForChange.calculateLeadTimeForChange();
+    changeFailureRate.calculateChangeFailureRate();
+    timeToRestoreService.calculateTimeToRestoreService();
+  }
 }
