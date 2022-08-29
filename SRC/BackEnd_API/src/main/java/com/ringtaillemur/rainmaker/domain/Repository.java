f281c2aca1c56dd128a877d6ada26379f03aaf31
inhyeokjo <68802559+inhyeokjo@users.noreply.github.com>
2022-08-29 14:58:46 +0900

    Refactoring backend server (#244)
    * fix: make createdDate updatable false
    
    * style: 스타일 가이드에 맞게 변경

--- SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/domain/Repository.java
+++ SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/domain/Repository.java
@@ -1,17 +1,10 @@
 package com.ringtaillemur.rainmaker.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 import lombok.AccessLevel;
 import lombok.Getter;
