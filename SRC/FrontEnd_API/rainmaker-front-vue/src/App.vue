ef213a138c2d780181793063e6f1a297af82b414
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2022-08-29 20:44:03 +0900

    hotfix: commits log 찍기

--- SRC/FrontEnd_API/rainmaker-front-vue/src/App.vue
+++ SRC/FrontEnd_API/rainmaker-front-vue/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex h-screen bg-gray-50 dark:bg-gray-900" id="app">
-    <Sidebar :isSideMenuOpen="isSideMenuOpen" @close-side-menu="isSideMenuOpen = false" />
+    <Sidebar :isSideMenuOpen="isSideMenuOpen" @close-side-menu="isSideMenuOpen = false"/>
 
     <div class="flex flex-col flex-1 w-full">
-      <Navigation @open-side-menu="isSideMenuOpen = !isSideMenuOpen" />
+      <Navigation @open-side-menu="isSideMenuOpen = !isSideMenuOpen"/>
       <div class="p-6 dark:text-white">
         <RouterView />
       </div>
@@ -12,6 +12,7 @@
 </template>
 
 <script setup lang="ts">
+
 import { ref } from "vue";
 import "@/assets/main.min.css"
 
@@ -20,4 +21,5 @@ import Sidebar from './components/Sidebar.vue'
 import Navigation from './components/Navigation.vue'
 
 const isSideMenuOpen = ref(false);
+
 </script>
