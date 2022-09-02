630b8cfc3b004bf5b205f02f52f736174268ae06
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2022-09-02 13:21:05 +0900

    Frontend login (#252)
    * fix: login 디자인 추가
    
    * feat: make register page
    
    * fix: 글씨체 변경
    
    * feat: styling 완료
    
    Co-authored-by: inhyeokjo <68802559+inhyeokjo@users.noreply.github.com>
    Co-authored-by: inhyeok <alzpqm@naver.com>

--- SRC/FrontEnd_API/rainmaker-front-vue/src/views/login.vue
+++ SRC/FrontEnd_API/rainmaker-front-vue/src/views/login.vue
@@ -1,10 +1,49 @@
 <template>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Jua&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@400;700&family=Jua&display=swap" rel="stylesheet">
+
   <div class="container grid px-6 mx-auto">
+
     <div class="login">
-      <div class="loginBox">
-        <div class="loginInnerBox">
-          RainMaker
-          <br>
+      <div>
+        <div class="githubExplain">
+          <div style="margin-bottom: 10px; " id="titleFont">
+            RainMaker
+          </div>
+          <hr id="line">
+          <div style="margin-bottom: 50px; padding: 0 30px" id="contentFont">
+            팀의 생산성 향상을 원하시고,
+            추적하시고 싶으시다면 로그인하세요. Git을 기반으로 이루어지기 때문에 필수적으로 Git의 접근권한이 필요합니다.
+          </div>
+          <div style="padding: 0 30px" id="contentFont">
+            오른쪽에 서비스로 편리하게 로그인, 회원가입하세요
+          </div>
+        </div>
+        <div class="loginBox">
+          <div class="logo">
+            <Logo/>
+          </div>
+          <div class="loginTitle" id="titleFont">
+            로그인
+          </div>
+          <div class="loginInnerBox">
+            <div class="githubIconBox">
+              <button>
+                <i class="fa fa-github" style="font-size:48px"></i>
+              </button>
+            </div>
+          </div>
+          <div class="githubLoginText" id="contentFont">
+            GitHub
+          </div>
+          <div class="githubRegister">
+            <button class="githubRegisterButton"  id="contentFont">
+              회원가입
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -12,8 +51,13 @@
 </template>
 
 <script>
+import Button from "./Buttons.vue";
+import Logo from "../components/Logo.vue"
 export default {
-
+  components: {
+    Button,
+    Logo,
+  }
 }
 </script>
 
@@ -34,7 +78,7 @@ export default {
 
 .login {
   -webkit-border-radius: 8px;
-  border-radius: 8px;
+  border-radius: 12px;
   border: 1px solid #dadce0;
   display: block;
   -webkit-flex-shrink: 0;
@@ -43,7 +87,7 @@ export default {
   min-height: 0;
   -webkit-transition: .2s;
   transition: .2s;
-  width: 450px;
+  width: 700px;
 }
 
 .login {
@@ -74,9 +118,72 @@ export default {
 
 .loginBox {
   box-sizing: inherit;
+  float: right;
+  width: 50%;
+  padding: 48px 40px 36px;
 }
 
 .loginInnerBox{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+
+}
+.githubIconBox{
+  border: solid 3px #c0bfbf;
+  border-radius: 10px;
+  padding: 5px 8px;
+}
+
+.githubExplain {
+  -webkit-border-bottom-left-radius: 8px;
+  -webkit-border-top-left-radius: 8px;
+  background: #c4ddff;
+  float: left;
+  width: 50%;
+  height: auto;
+  box-sizing: inherit;
+  min-height: 500px;
   padding: 48px 40px 36px;
 }
+
+.githubLoginText{
+  margin-bottom: 50px;
+  font-weight:bold;
+}
+
+.loginTitle {
+  margin-bottom: 20px;
+  font-size: 24px;
+}
+.logo{
+  margin-bottom: 50px;
+}
+#line {
+  width: 50%;
+  margin-left: auto;
+  margin-right: auto;
+  border: 0;
+  border-top: 3px solid;
+  margin-bottom: 20px;
+}
+
+.githubRegisterButton{
+  border: solid 3px #c0bfbf;
+  border-radius: 10px;
+  padding: 5px 8px;
+  width: 100px;
+}
+
+#contentFont {
+  font-family: 'Dongle', sans-serif;
+  font-size: 25px;
+}
+
+#titleFont {
+  font-family: 'Jua', sans-serif;
+  font-size: 25px;
+}
+
 </style>
