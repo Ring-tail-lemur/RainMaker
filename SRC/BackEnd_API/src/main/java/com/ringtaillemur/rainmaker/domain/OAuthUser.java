885dd33a8ef335f85ce540e22cd26250c5c7ad70
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-09-06 10:40:56 +0900

    add : BE : WebSecurity 생성 및 필요한 코드 완성 (#254)
    * add : BE : WebSecurity 생성
    
    * add : BE : Entity, Repository Interface 생성
    
    * add : BE : 유저 정보 Repository 저장 완료
    
    * add : BE : 유저 정보 session 저장 완료
    
    * add : BE : JPA 이용, Update 구현 완료
    
    * fix : BE : 쓸데없는 주석 삭제
    
    * add : BE : spring security localhost:3000으로 redirect 추가 (#258)
    
    * fix : fe : fix client_id
    
    * add : BE : spring security localhost:3000으로 redirect 추가 (#260)
    
    * add : BE : spring security 종현이 기분 좋아짐
    
    * hotfix : FE : 로그인 버튼 고침
    
    Co-authored-by: vidigummy <vidigummy@gamil.com>
    Co-authored-by: fakeVidi <108933006+fakeVidi@users.noreply.github.com>

---
+++ SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/domain/OAuthUser.java
@@ -0,0 +1,35 @@
+package com.ringtaillemur.rainmaker.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Setter
+@NoArgsConstructor
+@Table(name = "oauth_user")
+public class OAuthUser extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    @Column(name = "user_remote_id")
+    Long userRemoteId;
+    String name;
+    String oauthToken;
+    String url;
+    @Builder
+    public OAuthUser(Long id, String name, String url, String token){
+        this.userRemoteId = id;
+        this.name = name;
+        this.oauthToken = token;
+        this.url = url;
+    }
+
+    public OAuthUser update(String oauth_token){
+        this.oauthToken = oauth_token;
+        return this;
+    }
+}
