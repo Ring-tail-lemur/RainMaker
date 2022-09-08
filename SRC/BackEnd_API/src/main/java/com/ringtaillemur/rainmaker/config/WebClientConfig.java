93bcc0e07c706de46a33d086e3cd357f7f81eb6a
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2022-09-08 21:00:17 +0900

    feat: BaseUrl 전역 추가

--- SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/config/WebClientConfig.java
+++ SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/config/WebClientConfig.java
@@ -8,6 +8,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    public static final String frontEndBaseUrl= "http://127.0.0.1:3000";
+    public static final String backEndBaseUrl= "http://127.0.0.1:8080";
     @Bean
     public WebClient webClient() {
         return WebClient
