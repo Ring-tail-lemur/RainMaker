6a0444fb258f96cdf99613e064cfb0c69d3c7936
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-09-12 15:41:51 +0900

    refactoring : BE : OAuthContoller 리팩토링

--- SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/config/WebClientConfig.java
+++ SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/config/WebClientConfig.java
@@ -10,6 +10,8 @@ public class WebClientConfig {
 
     public static final String frontEndBaseUrl= "http://127.0.0.1:3000";
     public static final String backEndBaseUrl= "http://127.0.0.1:8080";
+    public static final String clientId = "8189c16057d124b9324e";
+    public static final String clientSecret = "e5231059eb31aa50d69a6a2154708a8a3f88954d";
     @Bean
     public WebClient webClient() {
         return WebClient
--- SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/controller/maindashboard/api/OAuthContoller.java
+++ SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/controller/maindashboard/api/OAuthContoller.java
@@ -1,157 +1,62 @@
 package com.ringtaillemur.rainmaker.controller.maindashboard.api;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.ringtaillemur.rainmaker.config.JwtTokenProvider;
-import com.ringtaillemur.rainmaker.config.Token;
 import com.ringtaillemur.rainmaker.config.UserAuthentication;
 import com.ringtaillemur.rainmaker.domain.OAuthUser;
 import com.ringtaillemur.rainmaker.domain.enumtype.OauthUserLevel;
 import com.ringtaillemur.rainmaker.repository.OAuthRepository;
-import com.ringtaillemur.rainmaker.service.oauth2.CustomOAuth2UserService;
 import com.ringtaillemur.rainmaker.service.oauth2.SecurityUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
 
 import javax.annotation.security.RolesAllowed;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.HashSet;
 import java.util.Optional;
-import java.util.Set;
 
-import static com.ringtaillemur.rainmaker.config.WebClientConfig.frontEndBaseUrl;
+import static com.ringtaillemur.rainmaker.config.WebClientConfig.*;
 
 @Controller
 public class OAuthContoller {
-    @Autowired
-    private HttpSession session;
+
     @Autowired
     private SecurityUserService securityUserService;
     @Autowired
     OAuthRepository oAuthRepository;
+
     @GetMapping("/login/oauth2/code/github")
     @RolesAllowed("FIRST_AUTH_USER")
     public String testMap2(@RequestParam(value = "code", required = false, defaultValue = "test")String code,
                            @RequestParam(value = "state", required = false, defaultValue = "test")String state,  RedirectAttributes redirectAttributes
     , HttpServletResponse res) throws IOException, URISyntaxException {
-        String clientId = "8189c16057d124b9324e";
-        String clientSecret = "e5231059eb31aa50d69a6a2154708a8a3f88954d";
-        System.out.println("code : " + code);
-        System.out.println("state : " + state);
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("client_id", clientId);
-        params.add("code", code);
-        params.add("client_secret", clientSecret);
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Accept","application/json");
-        HttpEntity<MultiValueMap<String,String>> entity = new HttpEntity<>(params, headers);
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.exchange("https://github.com/login/oauth/access_token", HttpMethod.POST, entity,String.class);
-        /*
-        access_token=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
-        scope=admin%3Aorg%2Cadmin%3Apublic_key%2Cadmin%3Arepo_hook%2Crepo
-        token_type=bearer
-        이렇게 온다.
-         */
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode jsonNode;
-        jsonNode = mapper.readTree(response.getBody());
-        String userAccessToken = String.valueOf(jsonNode.findValue("access_token"));
-        String refreshToken = String.valueOf(jsonNode.findValue("refresh_token"));
-        System.out.println(userAccessToken);
-        System.out.println("refresh_token : " + refreshToken);
-        System.out.println("Bearer ".concat(userAccessToken.replace("\"","")));
-        //여기까지가 Token 받아오는 부분
 
+        String userGithubToken = securityUserService.getUserGitHubOAuthToken(code);
 
+        //http GET을 통하여 Github 고유 JWT 가져오는 메서드
+        String userInfoLine = securityUserService.getUserInfoWithToken(userGithubToken);
+        // userInfoLine -> OauthUser로 변환(OauthUserLevel == FIRST_AUTH_USER)
+        OAuthUser nowLoginUser = securityUserService.stringToUserFirstAuthUserEntity(userInfoLine, userGithubToken.replace("\"",""));
+        //Repository 에 들어가 있는 상태의 OauthUser Entity 리턴
+        Optional<OAuthUser> nowUser = securityUserService.checkDuplicationAndCommitUser(nowLoginUser);
 
-        URL url = new URL("https://api.github.com/user");
-        HttpURLConnection http = (HttpURLConnection)url.openConnection();
-        http.setRequestProperty("Accept", "application/json");
-        http.setRequestProperty("Authorization", "Bearer ".concat(userAccessToken.replace("\"","")));
-        System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
-        BufferedReader bufferedReader = null;
-        String inputLine;
-        if(http.getResponseCode() > 199 && http.getResponseCode()<300){
-            bufferedReader = new BufferedReader(new InputStreamReader(http.getInputStream()));
-        }else{
-            bufferedReader = new BufferedReader(new InputStreamReader(http.getErrorStream()));
+        if(nowUser.isPresent()){
+            //Authentication 생성, JwtTokenProvider 생성하여 jwt Token 생성함, "sub" == remoteId, "ROLE" == UserLevel
+            String token = securityUserService.setJwtTokenWithUserInfo(nowUser.get());
+            //Bearer이라는 토큰 생성(jwt 토큰이 담김)
+            Cookie cookie = new Cookie("Bearer", token);
+            cookie.setPath("/");
+            res.addCookie(cookie);
         }
-        inputLine = bufferedReader.readLine();
-        http.disconnect();
-        OAuthUser inputOAuthUser = stringToJson(inputLine, userAccessToken.replace("\"",""));
-
-
-        OAuthUser nowUser = securityUserService.changeUserAuth(inputOAuthUser,OauthUserLevel.FIRST_AUTH_USER);
-
-        Authentication authentication = new UserAuthentication(String.valueOf(inputOAuthUser.getUserRemoteId()), null, null);
-        String token = JwtTokenProvider.generateToken(authentication, inputOAuthUser.getUser_level());
-
-        Cookie cookie = new Cookie("Bearer", token);
-        cookie.setPath("/");
-        System.out.println("GET DOMAIN ==============" +cookie.getDomain()) ;
-        res.addCookie(cookie);
-
         return "redirect:"+frontEndBaseUrl+"/RepositorySelect";
     }
 
-    public <T> OAuthUser stringToJson(String inputString, String OauthToken){
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.setPrettyPrinting();
-        Gson gson = gsonBuilder.create();
-
-        JsonUser user = gson.fromJson(inputString, JsonUser.class);
-        user.oauth_token = OauthToken;
-        System.out.println("user.oauth_token : "+user.oauth_token);
-
-
-        OAuthUser oAuthUser  = new OAuthUser(user.id, user.login, user.url, user.oauth_token, OauthUserLevel.FIRST_AUTH_USER);
-        try{
-            Optional<OAuthUser> tmpUser = oAuthRepository.findByUserRemoteId(oAuthUser.getUserRemoteId());
-            System.out.println("pres! : "+tmpUser.get().toString());
-            tmpUser.get().update(oAuthUser.getOauthToken());
-            oAuthRepository.save(tmpUser.get());
-        }catch (Exception e){
-            oAuthRepository.save(oAuthUser);
-        }
-
-        return oAuthUser;
-    }
-
-
-    private class JsonUser {
-        String oauth_token;
-        Long id;
-        String url;
-        String login;
-    }
-
-
-    @GetMapping("/test/tool")
-    public String testGet(){
-        System.out.println("dd");
-        return "test OK";
-    }
 }
