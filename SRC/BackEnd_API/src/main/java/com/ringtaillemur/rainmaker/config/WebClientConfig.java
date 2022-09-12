2b39581d6c9293b4f894d9f66bfc55117c37e47b
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-09-12 16:37:11 +0900

    add : BE : OauthApp Config 추가

--- SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/config/WebClientConfig.java
+++ SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/config/WebClientConfig.java
@@ -8,10 +8,18 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
-    public static final String frontEndBaseUrl= "http://127.0.0.1:3000";
-    public static final String backEndBaseUrl= "http://127.0.0.1:8080";
-    public static final String clientId = "8189c16057d124b9324e";
-    public static final String clientSecret = "e5231059eb31aa50d69a6a2154708a8a3f88954d";
+    //localTest용 세팅
+//    public static final String frontEndBaseUrl= "http://127.0.0.1:3000";
+//    public static final String backEndBaseUrl= "http://127.0.0.1:8080";
+//    public static final String clientId = "8189c16057d124b9324e";
+//    public static final String clientSecret = "e5231059eb31aa50d69a6a2154708a8a3f88954d";
+
+
+    //Public용 세팅
+    public static final String frontEndBaseUrl = "https://victorious-forest-095d4a310.1.azurestaticapps.net";
+    public static final String backEndBaseUrl = "https://spring-api-server.azurewebsites.net";
+    public static final String clientId = "42286a47489496b3129b";
+    public static final String clientSecret = "effa9cd720c3db113f3ce8f73a0d13ffd3a0633f";
     @Bean
     public WebClient webClient() {
         return WebClient
