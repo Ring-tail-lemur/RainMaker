8371045eca45e4d2b59928036f006f86061a6de6
inhyeok <alzpqm@naver.com>
2022-09-14 21:34:05 +0900

    fix: URL설정 오류 해결

--- SRC/FrontEnd_API/rainmaker-front-vue/src/main.ts
+++ SRC/FrontEnd_API/rainmaker-front-vue/src/main.ts
@@ -10,11 +10,6 @@ const app = createApp(App)
 
 // app.config.globalProperties.defaultURL = "http://localhost:8080";
 app.config.globalProperties.defaultURL = "https://spring-api-server.azurewebsites.net";
-app.config.globalProperties = {
-    defaultURL: "https://spring-api-server.azurewebsites.net",
-    clientId: "42286a47489496b3129b"
-};
-
 // app.config.globalProperties.clientId = "8189c16057d124b9324e"; // TEST 서버
 app.config.globalProperties.clientId = "42286a47489496b3129b"; // 실서버
 app.use(router)
