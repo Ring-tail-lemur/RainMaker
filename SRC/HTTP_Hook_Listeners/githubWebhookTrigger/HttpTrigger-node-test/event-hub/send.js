fd287399ded9e632c8deed1e40b6606a0268a7fc
vidigummy <vidigummy@gamil.com>
2022-09-14 23:15:09 +0900

    asf

--- SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/event-hub/send.js
+++ SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/event-hub/send.js
@@ -29,7 +29,7 @@ async function sender(cloudEventObj, context) {
 
 async function readJsonSecret(){
   //되라 이제..ㅇㅇdasㅁㄴㅇㄴㅇㅁasdf
-  const jsonFile = fs.readFileSync('/app-config.json','utf-8');
+  const jsonFile = fs.readFileSync('./app-config.json','utf-8');
   const jsonData = JSON.parse(jsonFile);
   return jsonData.eventHubConnectionString;
 }
