207cc58b1656f30b3bb0af521ca6416e7046a82e
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-09-15 11:14:19 +0900

    fix : action : 에라 모르겠다. 경로 설정 안 하련다.

--- SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/event-hub/send.js
+++ SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/event-hub/send.js
@@ -30,7 +30,7 @@ async function sender(cloudEventObj, context) {
 async function readJsonSecret(context){
   //되라 이제..test
   context.log("hihi");
-  const jsonFile = fs.readFileSync('app-config.json','utf-8');
+  const jsonFile = fs.readFileSync('event-hub-app-config.json','utf-8');
   context.log(jsonFIle)
   const jsonData = JSON.parse(jsonFile);
   
