f9f8117fb4939016dea4286b00b681cdc262d279
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-09-15 16:30:25 +0900

    test

--- SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/event-hub/send.js
+++ SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/event-hub/send.js
@@ -6,7 +6,7 @@ async function sender(cloudEventObj, context) {
   // const connectionString = 'Endpoint=sb://httptriggereventhubs.servicebus.windows.net/;SharedAccessKeyName=default;SharedAccessKey=ygtTa1wlgXx+UIr6up3i8x4aFHx2vNnD6NZ32K2W9gw=;EntityPath=githubhttpeventhub';
   const eventHubName = 'githubhttpeventhub';   
   // Create a producer client to send messages to the event hub.
-  const connectionString = readJsonSecret(context);
+  const connectionString = await readJsonSecret(context);
   try{
     const producer = new EventHubProducerClient(connectionString, eventHubName, {"retryDelayInMs":60000});
   
@@ -28,10 +28,7 @@ async function sender(cloudEventObj, context) {
 }
 
 async function readJsonSecret(context){
-  //되라 이제..test...ㄴtest...tesㄴㅁㄹㅇㄴㄹㅇ
-  // context.log("hihi");ß
   const jsonFile = fs.readFileSync('./event-hub-app-config.json','utf-8');
-  // context.log(jsonFile)
   console.log(jsonFile);
   const jsonData = JSON.parse(jsonFile);
   
