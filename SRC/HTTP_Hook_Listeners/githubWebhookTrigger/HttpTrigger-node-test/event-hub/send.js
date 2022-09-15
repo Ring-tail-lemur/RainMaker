1b4b9f221ad2e47bce1a187bdc2d6471bf768289
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-09-15 18:02:59 +0900

    fix : githubaction//WebhookTrigger : 작업 완료

--- SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/event-hub/send.js
+++ SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/event-hub/send.js
@@ -3,10 +3,7 @@ const fs = require("fs");
 const process = require("process");
 
 async function sender(cloudEventObj, context) {
-  
-  context.log(await listDir(process.cwd()));
 
-  // const connectionString = 'Endpoint=sb://httptriggereventhubs.servicebus.windows.net/;SharedAccessKeyName=default;SharedAccessKey=ygtTa1wlgXx+UIr6up3i8x4aFHx2vNnD6NZ32K2W9gw=;EntityPath=githubhttpeventhub';
   const eventHubName = 'githubhttpeventhub';   
   
   // Create a producer client to send messages to the event hub.
@@ -38,20 +35,10 @@ async function sender(cloudEventObj, context) {
 }
 
 async function readJsonSecret(context){
-  const path = context.FunctionAppDirectory;
-  context.log(path)
   const jsonFile = fs.readFileSync('.\\HttpTrigger-node-test\\event-hub\\event-hub-app-config.json','utf-8');
-  context.log(jsonFile);
   const jsonData = JSON.parse(jsonFile);
   return jsonData.eventHubConnectionString;
 }
 
 
-async function listDir(tdir) {
-  try {
-    return await fs.promises.readdir(tdir);
-  } catch (err) {
-    console.error('Error occurred while reading directory!', err);
-  }
-}
 module.exports.sender = sender;
