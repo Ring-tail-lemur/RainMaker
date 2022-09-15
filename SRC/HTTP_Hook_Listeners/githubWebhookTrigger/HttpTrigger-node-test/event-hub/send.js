380aeb64c17c5efe58aeea690a0dfc3657207738
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-09-15 11:34:22 +0900

    test : WT : test

--- SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/event-hub/send.js
+++ SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/event-hub/send.js
@@ -28,7 +28,7 @@ async function sender(cloudEventObj, context) {
 }
 
 async function readJsonSecret(context){
-  //되라 이제..test...test...
+  //되라 이제..test...test...test...
   context.log("hihi");
   const jsonFile = fs.readFileSync('event-hub-app-config.json','utf-8');
   context.log(jsonFIle)
