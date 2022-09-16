654fee406d7d378aab937508b7adfc59e71cbce4
inhyeok <alzpqm@naver.com>
2022-09-16 14:25:34 +0900

    fix: fix build error

--- SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/domain/Repository.java
+++ SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/domain/Repository.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
