5774b73701e0be4575afbe280306917f36e60798
inhyeok <alzpqm@naver.com>
2022-09-16 14:32:37 +0900

    fix: delete node module - gitignore

--- SRC/FrontEnd_API/vue-paper-dashboard-2-pro-v2.4.0/vue-paper-dashboard-2-pro-v2.4.0/node_modules/vee-validate/dist/types/components/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { ValidationProvider } from './Provider';
-export { ValidationObserver } from './Observer';
-export { withValidation } from './withValidation';
--- SRC/FrontEnd_API/vue-paper-dashboard-2-pro-v2.4.0/vue-paper-dashboard-2-pro-v2.4.0/node_modules/vee-validate/dist/types/rules/size.d.ts
+++
@@ -1,9 +0,0 @@
-import { RuleParamSchema } from '../types';
-declare const validate: (files: File | File[], { size }: Record<string, any>) => boolean;
-declare const params: RuleParamSchema[];
-export { validate, params };
-declare const _default: {
-    validate: (files: File | File[], { size }: Record<string, any>) => boolean;
-    params: RuleParamSchema[];
-};
-export default _default;
