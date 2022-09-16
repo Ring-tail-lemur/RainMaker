e360bc446efd1a155a338e9ec1f5cb9fa8e41b6d
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-09-16 17:06:57 +0900

    test for dynamic persistence

--- SRC/ETLs/analyst/src/main/java/com/ringtaillemur/analyst/TimerTriggerFunction.java
+++ SRC/ETLs/analyst/src/main/java/com/ringtaillemur/analyst/TimerTriggerFunction.java
@@ -7,7 +7,7 @@ import com.ringtaillemur.analyst.analysislogic.dorametric.ChangeFailureRate;
 import com.ringtaillemur.analyst.analysislogic.dorametric.LeadTimeForChange;
 import com.ringtaillemur.analyst.analysislogic.dorametric.TimeToRestoreService;
 import com.ringtaillemur.analyst.analysislogic.dorametric.UpdateCommitsReleaseId;
-// test27
+// test28
 public class TimerTriggerFunction {
 
 	LeadTimeForChange leadTimeForChange = LeadTimeForChange.getLeadTimeForChange();
