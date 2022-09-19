49113dc2a14738907c09ca238292645179718645
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-09-19 11:49:30 +0900

    test

--- SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/RainmakerWebserverApplication.java
+++ SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/RainmakerWebserverApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class RainmakerWebserverApplication {
 
-  //test 1
+  //test 2
   public static void main(String[] args) {
     SpringApplication.run(RainmakerWebserverApplication.class, args);
   }
