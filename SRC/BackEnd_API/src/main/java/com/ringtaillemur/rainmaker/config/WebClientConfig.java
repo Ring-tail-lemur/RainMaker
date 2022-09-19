1cd5eabddd2db9491504781d4f258f4c65fdd36a
vidigummy <vidigummy@gamil.com>
2022-09-19 13:39:06 +0900

    add : config : config

--- SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/config/WebClientConfig.java
+++ SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/config/WebClientConfig.java
@@ -16,7 +16,7 @@ public class WebClientConfig {
 	//    public static final String clientId = "8189c16057d124b9324e";
 	//    public static final String clientSecret = "e5231059eb31aa50d69a6a2154708a8a3f88954d";
 
-	//Public용 세팅
+	//Public용 세팅(안 건들여도 알아서 바꿔서 올라가니까 신경쓰지 말아줘요~!)
 //	public static final String frontEndBaseUrl = "https://victorious-forest-095d4a310.1.azurestaticapps.net";
 //	public static final String backEndBaseUrl = "https://spring-api-server.azurewebsites.net";
 	@Value("${spring.security.oauth2.client.registration.github.clientId}")
