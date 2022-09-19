a8887abaea5ceaf552fe8f8afe045b1346a0785b
inhyeok <alzpqm@naver.com>
2022-09-19 22:10:52 +0900

    feat: 에러페이지 수정

--- SRC/FrontEnd_API/vue-paper-dashboard-2-pro-v2.4.0/vue-paper-dashboard-2-pro-v2.4.0/src/components/GeneralViews/NotFoundPage.vue
+++ SRC/FrontEnd_API/vue-paper-dashboard-2-pro-v2.4.0/vue-paper-dashboard-2-pro-v2.4.0/src/components/GeneralViews/NotFoundPage.vue
@@ -13,9 +13,9 @@
                             description="We've created the marketing campaign of the website. It was a very interesting collaboration."
                             icon="nc-icon nc-alert-circle-i">
                 <h2 class="text-white mt-3">Page Not Found</h2>
-                <small class="text-white">Oops! It seems that this page does not exist.
+                <small class="text-white">페이지가 존재하지 않습니다
                   <br>
-                  You can navigate back <router-link to="/">to main page</router-link>
+                  돌아가기 : <router-link to="/">메인 페이지</router-link>
                 </small>
               </info-section>
             </div>
