34a959a9a0c79877c1ab561c30a05c5de892c7dc
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-09-26 22:33:40 +0900

    TEST

--- SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/release/releaseMainModule.js
+++ SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/release/releaseMainModule.js
@@ -27,16 +27,6 @@ async function releaseMain(hookBody, cloudEventObj, context) {
         context.log(cloudEventObj.action + " is not yet prepared");
     }
     
-    // if(cloudEventObj.action == 'created'){
-    //     return await createdModule.createdMain(hookBody, cloudEventObj, context);
-    // }else if(cloudEventObj.action == 'released'){
-        
-    // }else if(cloudEventObj.action == 'prereleased'){
 
-    // }else if(cloudEventObj.action == 'published'){
-
-    // }else{
-
-    // }
 }
 module.exports.releaseMain = releaseMain;
