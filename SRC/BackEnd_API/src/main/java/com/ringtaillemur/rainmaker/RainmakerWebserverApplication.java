e5c9ad4a5656fca928fdb58508aa5f1a62b1f76a
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-09-26 19:59:13 +0900

    Git ignore GitHub action (#296)
    * fix : actions : BE Deployment 전략 변경
    
    * test for action[DEPLOY]
    
    * fix : actions : DEPLOY 전략 변경
    
    * test for action
    
    * fix : action : 상대경로가 안 먹기 시작했다 큰일 났다.
    
    * Test for action deploy
    
    * fix : actions : variable-subtitution이 왜 파일을 못 찾을까?
    
    * test for action[DEPLOY]
    
    * fix : action : subtitution 길 잃음 ㅎㅎ
    
    * Test For action Deploy
    
    * fix : action : DEPLOY에서 substitution이 안 먹어요
    
    * TEST FOR DEPLOY ACTION
    
    * fix : action : Deploy substitution 절대 경로 넣기
    
    * TEST FOR DEPLOY ACTION
    
    * fix : actions : 쌍괄호를 해야하는데 홀괄호를 해서 현타가 오네요.
    
    * TEST FOR DEPLOY ACTIONS
    
    * done : actions : deployment 변환 완료
    
    * fix : action : ETL Pipeline 수정 시작
    
    * delete : ETLs : Delete persistence.xml
    
    * fix : actions : 와 resources까지 없어져뿌네
    
    * TEST FOR DEPLOY ACTIONS
    
    * TEST FOR ACTIONS DEPLOY
    
    * fix : action : 상대경로로 변경
    
    * TEST FOR DEPLOY ACTIONS
    
    * done : actions : ETLs Deployment pipeline done
    
    * delete : HC : delete persistence.xml
    
    * fix : actions : HC pipeline 수정
    
    * TEST FOR DEPLOY ACTIONS
    
    * fix : actions : HC deployment actions
    
    * TEST FOR DEPLOYMENT ACTIONS
    
    * done : actions : HC deployment done

--- SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/RainmakerWebserverApplication.java
+++ SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/RainmakerWebserverApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class RainmakerWebserverApplication {
 
-  //test 2
+  //test 10
   public static void main(String[] args) {
     SpringApplication.run(RainmakerWebserverApplication.class, args);
   }
