121b929c38fbf806a398b4847b6e55418bfe6b05
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-09-26 22:11:43 +0900

    delete : Listener : node_modules gitIgnore (#299)

---
+++ SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/utils/labelCreateRestApi.js
@@ -0,0 +1,30 @@
+const { Octokit } = require("@octokit/core");
+// const msSQLPool = require('../ms-sql/msSQLPool')
+
+async function createGitHubLabel(releaseName, repositoryId, repositoryName, ownerName, token, context) {
+    const octokit = new Octokit({
+        auth: token
+    })
+    let response;
+    try {
+        response = await octokit.request(`POST /repos/${ownerName}/${repositoryName}/labels`, {
+            owner: ownerName,
+            repo: repositoryName,
+            name: `[RainMaker]runtime-error-${releaseName}`,
+            description: 'RainMaker runtime error label',
+            color: 'f213be'
+        });
+    } catch (e){
+        if (e.status === 401) {
+            throw "토큰 인증 에러";
+        } else if(e.status === 422) {
+            throw "같은 이름의 Label이 이미 존재합니다.";
+            //처리해야함
+        } else if(e.status === 404) {
+            context.log(`POST /repos/${ownerName}/${repositoryName}/labels 요청이 유효하지 않습니다.`);
+            throw "주소가 유효하지 않음";
+        }
+    }
+}
+
+module.exports.createGitHubLabel = createGitHubLabel;
