a601f013c150822aa573a7b7f1846a1c9c47277b
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-09-26 20:32:31 +0900

    WebClientConfig 중요 정보 삭제

--- SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/config/WebClientConfig.java
+++ SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/config/WebClientConfig.java
@@ -7,23 +7,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
-
-	//localTest용 세팅
-	@Value("${spring.dev.frontendUri}")
-	public static String frontEndBaseUrl;
-	@Value("${spring.dev.backendUri}")
-	public static String backEndBaseUrl;
-	//    public static final String clientId = "8189c16057d124b9324e";
-	//    public static final String clientSecret = "e5231059eb31aa50d69a6a2154708a8a3f88954d";
-
-	//Public용 세팅(안 건들여도 알아서 바꿔서 올라가니까 신경쓰지 말아줘요~!)
-//	public static final String frontEndBaseUrl = "https://victorious-forest-095d4a310.1.azurestaticapps.net";
-//	public static final String backEndBaseUrl = "https://spring-api-server.azurewebsites.net";
-	@Value("${spring.security.oauth2.client.registration.github.clientId}")
-	public static String clientId = "8189c16057d124b9324e";
-	@Value("${spring.security.oauth2.client.registration.github.clientSecret}")
-	public static String clientSecret = "236a8cb553d45466f0248ddf83b57466c0c7651d";
-
 	@Bean
 	public WebClient webClient() {
 		return WebClient
