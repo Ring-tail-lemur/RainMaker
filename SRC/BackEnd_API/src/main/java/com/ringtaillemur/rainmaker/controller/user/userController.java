9be8e7f52c3a1d90cb24b6cbbcf75ad126eddb3f
inhyeok <alzpqm@naver.com>
2022-09-27 20:20:49 +0900

    feat: 토큰 입력 페이지 그리기

---
+++ SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/controller/user/userController.java
@@ -0,0 +1,8 @@
+package com.ringtaillemur.rainmaker.controller.user;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class userController {
+
+}
---
+++ SRC/FrontEnd_API/vue-paper-dashboard-2-pro-v2.4.0/vue-paper-dashboard-2-pro-v2.4.0/src/components/Dashboard/Views/Pages/TokenSetting.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="register-page">
+    <app-navbar></app-navbar>
+    <div class="wrapper wrapper-full-page ">
+      <div class="full-page register-page section-image" filter-color="black">
+        <div class="content">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-4 col-md-6 mr-auto ml-auto">
+                <card type="signup" class="text-center h-full">
+                  <template slot="header">
+                    <h4 class="card-title mt-4">Github Token</h4>
+                  </template>
+                  <div class="text-left ">
+                    1. <a href="https://github.com/settings/tokens/new">GitHub Token 발급 페이지</a>에 들어가세요.<br>
+                    2. 아래의 스코프를 <b>꼭</b> 체크해주세요!<br>
+                    <div class="text-center m-3">
+                      <span class="github-webhook-scope">read:discussion</span>
+                      <span class="github-webhook-scope">read:org</span>
+                      <span class="github-webhook-scope">read:user</span>
+                      <span class="github-webhook-scope">repo</span>
+                      <span class="github-webhook-scope">user:email</span>
+                      <span class="github-webhook-scope">read:repo_hook</span>
+                      <span class="github-webhook-scope">write:repo_hook</span>
+                    </div>
+                  </div>
+                  <p slot="footer" type="info">
+                    <input v-model="githubToken" class="w-11/12 mt-3" style="width: 100%; margin-bottom: 20px; height: 40px" type="text" placeholder="github token을 입력해주세요!" ></input>
+                    <p-button round block class="mb-3" v-on:click="saveToken()">확인</p-button>
+                  </p>
+                </card>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="full-page-background" style="background-image: url(static/img/background/background-devops-2.jpg)"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {Button, Card, Checkbox, InfoSection} from "@/components/UIComponents";
+import AppNavbar from "@/components/Dashboard/Views/Pages/Layout/AppNavbar";
+import AppFooter from "@/components/Dashboard/Views/Pages/Layout/AppFooter";
+import axios from "axios";
+import setHeaderJWT from "@/api/setHeaderJWT";
+
+export default {
+  name: "TokenSetting",
+  components: {
+    Card,
+    AppNavbar,
+    AppFooter,
+    InfoSection,
+    [Checkbox.name]: Checkbox,
+    [Button.name]: Button
+  },
+  data() {
+    return {
+      githubToken: 'hello'
+    }
+  },
+  methods: {
+    async saveToken() {
+      await axios({
+        headers: setHeaderJWT(),
+        method: "post",
+        url: this.custom.defaultURL + "/token",
+        data : {
+          token: this.githubToken
+        }
+      });
+    }
+  },
+  beforeDestroy() {
+    this.closeMenu()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
