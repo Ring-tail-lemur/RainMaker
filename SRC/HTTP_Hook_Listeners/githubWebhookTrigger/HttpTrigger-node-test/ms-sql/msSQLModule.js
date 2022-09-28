69d85ba827a232cb2fdff15a3e8cd117cbf86397
vidigummy <vidigummy@gamil.com>
2022-09-28 23:25:23 +0900

    fix : GL : query 문제

--- SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/ms-sql/msSQLModule.js
+++ SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/ms-sql/msSQLModule.js
@@ -28,14 +28,16 @@ async function executeSqlQuery(query){
 }
 
 async function getTokenByRepositoryId(repositoryId,context){
-    newQuery = `SELECT DISTINCT oauth_token FROM oauth_user WHERE user_remote_id = (SELECT DISTINCT oauth_user_id FROM oauth_user_repository_table WHERE repository_id =  ${repositoryId})`;
-    const queryResult =  await executeSqlQuery(newQuery); 
-    // context.log(queryResult[0][0]['oauth_token'])
+    newQuery = `SELECT DISTINCT oauth_token FROM oauth_user WHERE user_remote_id = (SELECT TOP 1 oauth_user_id FROM oauth_user_repository_table WHERE repository_id =  ${Number(repositoryId)})`;
+    console.log(newQuery);
+    const queryResult =  await executeSqlQuery(newQuery);
+    console.log(queryResult) 
+    console.log(queryResult[0][0]['oauth_token'])
     // context.log(queryResult);
     return queryResult[0][0]['oauth_token']
 }
 
-getTokenByRepositoryId(514871723,'');
+getTokenByRepositoryId('528356870','');
 // console.log(result);
 
 module.exports.getTokenByRepositoryId = getTokenByRepositoryId;
