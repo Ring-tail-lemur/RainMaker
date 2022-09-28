988ec07e62ed0f9853407aa4538fae6785b8df5c
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-09-28 20:40:09 +0900

    trans : GL : 기기 이동을 위한 commit

---
+++ SRC/HTTP_Hook_Listeners/githubWebhookTrigger/HttpTrigger-node-test/ms-sql/msSQLModule.js
@@ -0,0 +1,40 @@
+const sql = require("mssql");
+const fs = require("fs");
+async function readJsonSecret(context){
+    const jsonFile = fs.readFileSync('../ms-sql-config.json','utf-8');
+    const config = JSON.parse(jsonFile);
+    return config;
+}
+
+
+
+async function executeSqlQuery(query){
+    sqlConfig =  await readJsonSecret('hi');
+    // console.log(query);
+    // console.log(sqlConfig);
+    try{
+        // make sure that any items are correctly URL encoded in the connection string
+        const connectionPool = new sql.ConnectionPool(sqlConfig);
+        await connectionPool.connect();
+        const result = await connectionPool.request().query(query);
+        if(result.recordsets != null){
+            return result.recordsets;
+        }else{
+            throw new Error;
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function getTokenByRepositoryId(repositoryId,context){
+    newQuery = `SELECT DISTINCT oauth_token FROM oauth_user WHERE user_remote_id = (SELECT DISTINCT oauth_user_id FROM oauth_user_repository_table WHERE repository_id =  ${repositoryId})`;
+    const queryResult =  await executeSqlQuery(newQuery); 
+    console.log(queryResult[0][0]['oauth_token'])
+    return queryResult[0][0]['oauth_token']
+}
+
+getTokenByRepositoryId(514871723,'');
+// console.log(result);
+
+module.exports.getTokenByRepositoryId = getTokenByRepositoryId;
