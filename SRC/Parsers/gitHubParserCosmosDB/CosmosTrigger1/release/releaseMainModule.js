ac8e9b58872f7433a782fa3280f9eb376ad2aa74
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2022-09-29 14:05:32 +0900

    hotfix: release delete 안됨 이슈, log 추가

--- SRC/Parsers/gitHubParserCosmosDB/CosmosTrigger1/release/releaseMainModule.js
+++ SRC/Parsers/gitHubParserCosmosDB/CosmosTrigger1/release/releaseMainModule.js
@@ -3,10 +3,11 @@ const labelCreateRestApi = require('../restApiModule/labelCreateRestApi');
 
 async function releaseMain(pool, eventObject, context) {
 
-
+    context.log(eventObject.action, "1");
     if(eventObject.action === "created") {
         await releaseRepository.insertReleaseByUserIdAndTagIdAndRepoId(pool, context, eventObject.X_GitHub_Delivery, eventObject.release_author_id, eventObject.release_tag_name, eventObject.repository_id, eventObject.release_id, eventObject.pre_release, eventObject.release_name, eventObject.published_at, eventObject.draft, eventObject.action);
     } else if (eventObject.action === "deleted") {
+        context.log(eventObject.action, "2");
         await releaseRepository.deleteReleaseByReleaseId(pool, context, eventObject.release_id);
     }
 
