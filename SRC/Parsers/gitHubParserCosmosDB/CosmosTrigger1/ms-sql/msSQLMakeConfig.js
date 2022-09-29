e3b3bb79d1f1b3bfe0312bccc7ba220545b5519a
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2022-09-29 14:29:56 +0900

    hotfix : GP : 새로운 config파일 가지고 config만들기.

--- SRC/Parsers/gitHubParserCosmosDB/CosmosTrigger1/ms-sql/msSQLMakeConfig.js
+++ SRC/Parsers/gitHubParserCosmosDB/CosmosTrigger1/ms-sql/msSQLMakeConfig.js
@@ -3,10 +3,10 @@ async function makeConfig(context){
     const ConfigClass = await readJsonSecret(context);
     msConfig = {
         port : parseInt(JSON.stringify(ConfigClass.port).replace(/['"]+/g, '')),
-        user : JSON.stringify(ConfigClass.user).replace(/['"]+/g, ''),
-        password : JSON.stringify(ConfigClass.password).replace(/['"]+/g, ''),
+        user : JSON.stringify(ConfigClass.authentication.userName).replace(/['"]+/g, ''),
+        password : JSON.stringify(ConfigClass.authentication.password).replace(/['"]+/g, ''),
         server : JSON.stringify(ConfigClass.server).replace(/['"]+/g, ''),
-        database : JSON.stringify(ConfigClass.database).replace(/['"]+/g, ''),
+        database : JSON.stringify(ConfigClass.options.database).replace(/['"]+/g, ''),
         options: {
             encrypt: true, // Use this if you're on Windows Azure
         },
@@ -18,7 +18,7 @@ async function makeConfig(context){
 }
 async function readJsonSecret(context){
     // const jsonData = require('../ms-sql-config.json');
-    const jsonFile = fs.readFileSync('.\\CosmosTrigger1\\ms-sql-config.json','utf-8');
+    const jsonFile = fs.readFileSync('.\\ms-sql-config.json','utf-8');
     const jsonData = await JSON.parse(jsonFile);
     return jsonData;
 }
