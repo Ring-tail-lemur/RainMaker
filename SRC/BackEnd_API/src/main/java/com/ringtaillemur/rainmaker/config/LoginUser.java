2d99257e61665726dfa41e13c763fd9f7c70a3d8
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-10-04 12:29:40 +0900

    add : BE : logout logic 생성

--- SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/config/LoginUser.java
+++ SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/config/LoginUser.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 
 import com.ringtaillemur.rainmaker.domain.OAuthUser;
+import com.ringtaillemur.rainmaker.domain.enumtype.OauthUserLevel;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -11,10 +12,12 @@ import lombok.Setter;
 @Getter
 @Setter
 public class LoginUser extends User {
-	private OAuthUser oAuthUser;
+	private Long userRemoteId;
+	private OauthUserLevel userLevel;
 
 	public LoginUser(OAuthUser oAuthUser){
 		super(oAuthUser.getUserRemoteId().toString(), oAuthUser.getName(), AuthorityUtils.createAuthorityList(oAuthUser.getUserLevel().toString()));
-		this.oAuthUser = oAuthUser;
+		this.userRemoteId = oAuthUser.getUserRemoteId();
+		this.userLevel = oAuthUser.getUserLevel();
 	}
 }
