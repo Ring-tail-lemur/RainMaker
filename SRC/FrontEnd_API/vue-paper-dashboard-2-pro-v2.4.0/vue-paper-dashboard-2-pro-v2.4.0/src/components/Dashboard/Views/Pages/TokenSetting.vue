c22129adfa0da4c8ff7d72e31c4ed3f094ca0e16
inhyeok <alzpqm@naver.com>
2022-10-10 23:11:43 +0900

    feat: frount에서 token의 유효성 확인

--- SRC/FrontEnd_API/vue-paper-dashboard-2-pro-v2.4.0/vue-paper-dashboard-2-pro-v2.4.0/src/components/Dashboard/Views/Pages/TokenSetting.vue
+++ SRC/FrontEnd_API/vue-paper-dashboard-2-pro-v2.4.0/vue-paper-dashboard-2-pro-v2.4.0/src/components/Dashboard/Views/Pages/TokenSetting.vue
@@ -12,7 +12,8 @@
                     <h4 class="card-title mt-4">Github Token</h4>
                   </template>
                   <div class="text-left ">
-                    1. <a href="https://github.com/settings/tokens/new">GitHub Token 발급 페이지</a>에 들어가세요.<br>
+                    1. <a href="https://github.com/settings/tokens/new" target="_blank">GitHub Token 발급 페이지</a>에
+                    들어가세요.<br>
                     2. 아래의 스코프를 <b>꼭</b> 체크해주세요!<br>
                     <div class="text-center m-3">
                       <span class="github-webhook-scope">read:discussion</span>
@@ -25,10 +26,14 @@
                     </div>
                   </div>
                   <p slot="footer" type="info">
-                    <input v-model="githubToken" class="w-11/12 mt-3"
+                    <b><span style="" v-bind:style="{color:getBorderColor()}">{{ checkString }}</span></b>
+                    <input v-model="githubToken"
+                           v-bind:style="{borderColor:getBorderColor()}"
+                           class="w-11/12 mt-3"
+                           @blur="verifyToken()"
                            style="width: 100%; margin-bottom: 20px; height: 40px" type="text"
                            placeholder="github token을 입력해주세요!"></input>
-                    <p-button round block class="mb-3" v-on:click="saveToken()">확인</p-button>
+                    <p-button round block class="mb-3" v-on:click="saveToken">확인</p-button>
                   </p>
                 </card>
               </div>
@@ -61,7 +66,9 @@ export default {
   },
   data() {
     return {
-      githubToken: ''
+      githubToken: '',
+      tokenValidation: 'start',
+      checkString: ''
     }
   },
   methods: {
@@ -75,7 +82,37 @@ export default {
         }
       });
       window.location.replace(this.custom.myURL + "/RepositorySelect");
-    }
+    },
+    verifyToken() {
+      axios({
+        url: "https://api.github.com/user",
+        method: "get",
+        headers: {
+          Authorization: `Bearer ${this.githubToken}`
+        }
+      }).then(response => {
+        if (response.status === 200) {
+          this.tokenValidation = true;
+          this.checkString = '토큰이 확인되었습니다!'
+        } else {
+          this.tokenValidation = false;
+          this.checkString = '토큰을 다시 확인해주세요!'
+        }
+      }).catch(error => {
+        this.tokenValidation = false;
+        this.checkString = '토큰을 다시 확인해주세요!'
+      });
+    },
+    getBorderColor() {
+      if (this.tokenValidation === 'start') {
+        return 'basic'
+      }
+      if (this.tokenValidation === true) {
+        return 'green';
+      }
+      return 'red';
+    },
+
   },
   beforeDestroy() {
     this.closeMenu()
