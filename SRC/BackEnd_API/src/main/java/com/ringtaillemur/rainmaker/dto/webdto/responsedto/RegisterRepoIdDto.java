b9bfd701c87da75c356296e82c438069b4656af8
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2022-10-13 16:20:25 +0900

    fix: 저장하기 누르는 경우 대기화면을 띄우도록 변경

--- SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/dto/webdto/responsedto/RegisterRepoIdDto.java
+++ SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/dto/webdto/responsedto/RegisterRepoIdDto.java
@@ -6,13 +6,16 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
 public class RegisterRepoIdDto {
 
     @NotNull
+    @Size(min = 1)
     List<String> repoIds;
 
     public RegisterRepoIdDto() {
