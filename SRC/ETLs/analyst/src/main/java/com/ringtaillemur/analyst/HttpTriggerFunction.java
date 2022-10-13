c7d7de637a5d66ab76fc6f511890bcd1ec47a0b4
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-10-13 11:29:26 +0900

    TEST : ETL : DEPLOY TEST

--- SRC/ETLs/analyst/src/main/java/com/ringtaillemur/analyst/HttpTriggerFunction.java
+++ SRC/ETLs/analyst/src/main/java/com/ringtaillemur/analyst/HttpTriggerFunction.java
@@ -1,8 +1,5 @@
 package com.ringtaillemur.analyst;
 
-import java.util.List;
-import java.util.Optional;
-
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
@@ -11,19 +8,23 @@ import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
+import java.util.List;
+import java.util.Optional;
 
 public class HttpTriggerFunction {
-	@FunctionName("AnalystHttpTrigger")
-	public HttpResponseMessage run(
-		@HttpTrigger(name = "req",
-			methods = {HttpMethod.GET, HttpMethod.POST},
-			authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
-		final ExecutionContext context) throws Exception {
-		TimerTriggerFunction timerTriggerFunction = new TimerTriggerFunction();
-		timerTriggerFunction.run("HistoryCollector에 의해 호출", context);
-		return request
-			.createResponseBuilder(HttpStatus.OK)
-			.body("happy! Done!")
-			.build();
-	}
+
+  @FunctionName("AnalystHttpTrigger")
+  public HttpResponseMessage run(
+    @HttpTrigger(
+      name = "req",
+      methods = { HttpMethod.GET, HttpMethod.POST },
+      authLevel = AuthorizationLevel.ANONYMOUS
+    ) HttpRequestMessage<Optional<String>> request,
+    final ExecutionContext context
+  )
+    throws Exception {
+    TimerTriggerFunction timerTriggerFunction = new TimerTriggerFunction();
+    timerTriggerFunction.run("HistoryCollector에 의해 호출", context);
+    return request.createResponseBuilder(HttpStatus.OK).body("happy!").build();
+  }
 }
