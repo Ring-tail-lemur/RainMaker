e122303b626c5694263b26ddab22c491b0e89fe2
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-10-25 13:19:08 +0900

    fix : EL : ?

--- SRC/EventHubTrigger/eventhubtrigger/blob/blob-extractor.js
+++ SRC/EventHubTrigger/eventhubtrigger/blob/blob-extractor.js
@@ -1,29 +1,32 @@
 const axios = require('axios');
-async function blobModule(fileUrl){
+async function blobModule(fileUrl,context){
     const options = '{"responseType": "blob"}';
     let objList = [];
     const response = await axios.get(fileUrl,options);
-
     const avroFile = response.data;
-    let arr = avroFile.split('{');
-    for (let i = 0; i < arr.length; i++) {
-        let a = arr[i].replace(/['}]+/g, '');
-        if(a.startsWith('"hook_event')){
-            let nowObj = {};
-            let splitByComma = a.split(",");
-            for(let i = 0; i < splitByComma.length; i++) {
-                nowLine = splitByComma[i];
-                if(nowLine.startsWith("\"")){
-                    lastLineSplit = nowLine.split("\"");
-                    indexDoublePoint = lastLineSplit.indexOf(":");
-                    nowObj[lastLineSplit[indexDoublePoint-1]] = lastLineSplit[indexDoublePoint+1]; 
+    try{
+        let arr = avroFile.split('{');
+        for (let i = 0; i < arr.length; i++) {
+            let a = arr[i].replace(/['}]+/g, '');
+            if(a.startsWith('"hook_event')){
+                let nowObj = {};
+                let splitByComma = a.split(",");
+                for(let i = 0; i < splitByComma.length; i++) {
+                    nowLine = splitByComma[i];
+                    if(nowLine.startsWith("\"")){
+                        lastLineSplit = nowLine.split("\"");
+                        indexDoublePoint = lastLineSplit.indexOf(":");
+                        nowObj[lastLineSplit[indexDoublePoint-1]] = lastLineSplit[indexDoublePoint+1]; 
+                    }
                 }
+                objList.push(nowObj);
             }
-            objList.push(nowObj);
         }
+    }catch(e){
+        context.log(e);
     }
+    
     return objList;
 }
 
-
-exports.module.blobModule = blobModule;
+module.exports.blobModule = blobModule;
--- SRC/EventHubTrigger/eventhubtrigger/index.js
+++ SRC/EventHubTrigger/eventhubtrigger/index.js
@@ -1,8 +1,14 @@
 const blobExtractor = require('./blob/blob-extractor.js');
 module.exports = async function (context, eventGridEvent) {
-    const fileUrl = eventGridEvent.data.fileUrl;
-    context.log(fileUrl);
-    const objList = blobExtractor.blobModule(fileUrl);
-    context.log(objList);
+    // context.log(eventGridEvent);
+    try{
+        const fileUrl = eventGridEvent.data.fileUrl;
+        context.log(fileUrl);
+        const objList = await blobExtractor.blobModule(fileUrl,context);
+        context.log(objList);
+    }catch(e){
+        context.log(e);
+        context.log(eventGridEvent)
+    }
 };
 
