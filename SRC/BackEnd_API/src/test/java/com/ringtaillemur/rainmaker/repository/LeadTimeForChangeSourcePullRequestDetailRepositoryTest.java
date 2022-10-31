3a94e7a056a383f1ba7528996b2731a564db5426
inhyeok <alzpqm@naver.com>
2022-10-31 19:06:23 +0900

    test: add repositories test

--- SRC/BackEnd_API/src/test/java/com/ringtaillemur/rainmaker/repository/LeadTimeForChangeSourcePullRequestDetailRepositoryTest.java
+++ SRC/BackEnd_API/src/test/java/com/ringtaillemur/rainmaker/repository/LeadTimeForChangeSourcePullRequestDetailRepositoryTest.java
@@ -1,4 +1,23 @@
+package com.ringtaillemur.rainmaker.repository;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ringtaillemur.rainmaker.domain.view.LeadTimeForChangeSourcePullRequestDetail;
+
+@SpringBootTest
 class LeadTimeForChangeSourcePullRequestDetailRepositoryTest {
-  
+	@Autowired
+	LeadTimeForChangeSourcePullRequestDetailRepository leadTimeForChangeSourcePullRequestDetailRepository;
+
+	@Test
+	void test() {
+		List<LeadTimeForChangeSourcePullRequestDetail> all = leadTimeForChangeSourcePullRequestDetailRepository.findAll();
+	}
+
 }
