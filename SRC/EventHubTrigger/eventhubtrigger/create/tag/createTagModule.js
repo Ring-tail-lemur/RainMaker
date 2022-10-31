0cb0448923139b277316c8204203e46d0e6b3e8d
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-10-31 10:40:49 +0900

    정리

--- SRC/EventHubTrigger/eventhubtrigger/create/tag/createTagModule.js
+++
@@ -1,7 +0,0 @@
-const createTagRepository = require('./createTagRepository');
-
-async function createTagMain(pool, eventObject) {
-    await createTagRepository.insertTagByRepoIdAndUserId(pool, eventObject.X_GitHub_Delivery, eventObject.ref_name, eventObject.repository_id, eventObject.owner_id);
-}
-
-module.exports.createTagMain = createTagMain;
--- SRC/EventHubTrigger/eventhubtrigger/delete/branch/deleteBranchModule.js
+++
@@ -1,8 +0,0 @@
-const deleteBranchRepository = require("./deleteBranchRepository");
-
-async function pullRequestCloseMain(pool, eventObject, context) {
-
-    await deleteBranchRepository.updateBranchByRepoIdAndUserId(pool, context, eventObject.ref_name, eventObject.ref_repository_id, eventObject.ref_owner_id);
-}
-
-module.exports.pullRequestCloseMain = pullRequestCloseMain;
