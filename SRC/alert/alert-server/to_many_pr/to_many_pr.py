5ee99a4ed1987e962dde80f1ce055693b298f33d
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-11-01 15:39:46 +0900

    :tada: Alert Server Extractor 구조 완성

--- SRC/alert/alert-server/to_many_pr/to_many_pr.py
+++ SRC/alert/alert-server/to_many_pr/to_many_pr.py
@@ -1,11 +1,10 @@
-from pull_request import pull_request_main
+from . import pull_request_main
+from . import repository_main
 from datetime import datetime
 import datetime as dt
 import pandas as pd
-class DataModule():
-
+class PrExtractor():
     pr = pull_request_main.PullRequest()
-    
     now = datetime.now()
     week_ago = now+dt.timedelta(weeks=-1)
     month_ago = now+dt.timedelta(weeks=-5)
@@ -13,7 +12,17 @@ class DataModule():
     week_ago = week_ago.strftime("%Y-%m-%d")
     month_ago = month_ago.strftime("%Y-%m-%d")
 
-    def get_above_average(self):
+    def get_user_above_average(self):
+        rp = repository_main.Repository()
+        result_df = self.get_repository_pr_cnt()
+        burn_out_repository = result_df['repository_id'].values.tolist()
+        # print(burn_out_repository)
+        burn_out_user_list = rp.get_repo_and_user_info_list(burn_out_repository)
+        return burn_out_user_list
+        # print(burn_out_user_list)
+
+
+    def get_repository_pr_cnt(self):
         # 레포 리스트
         repository_list = self.pr.get_repository_id_list()
         # 풀리케 리스트
@@ -31,15 +40,16 @@ class DataModule():
             self.month_ago_pr(repo_pr_df)
             # TODO: 현재는 그냥 현재/7 > 지난 한 달 / 28 이면 True 반환중. 개선 필요
             repo_pr_len_list = self.make_repository_cnt()
-            repo_pr_len_list[0] = repository
-            new_data = {
-                'repository_id' : [repo_pr_len_list[0]],
-                'now' : [repo_pr_len_list[1]],
-                'last' : [repo_pr_len_list[2]]
-            }
-            new_df = pd.DataFrame(new_data)
-            result_df = pd.concat([result_df, new_df])
-        print(result_df)
+            if(repo_pr_len_list[1]/7 > repo_pr_len_list[2]/28):
+                repo_pr_len_list[0] = repository
+                new_data = {
+                    'repository_id' : [repo_pr_len_list[0]],
+                    'now' : [repo_pr_len_list[1]],
+                    'last' : [repo_pr_len_list[2]]
+                }
+                new_df = pd.DataFrame(new_data)
+                result_df = pd.concat([result_df, new_df])
+        return result_df
 
     def this_week_pr(self, repo_pr_df):
         cond_this_week_end = repo_pr_df.created_date <= self.now
@@ -55,8 +65,8 @@ class DataModule():
     def make_repository_cnt(self):
         return [0, len(self.last_week_pr_list), len(self.last_month_pr_list)]
 
-test = DataModule()
-test.get_above_average()
+# test = PrExtractor()
+# test.get_user_above_average()
 # print(result_list)
 
         
