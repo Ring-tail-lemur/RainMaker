7dddfef1ae96767ead23f4dc45b0c51c225ae689
inhyeok <alzpqm@naver.com>
2022-11-02 13:53:14 +0900

    feat: append PullRequest Domain

---
+++ SRC/BackEnd_API/src/main/java/com/ringtaillemur/rainmaker/domain/PullRequest.java
@@ -0,0 +1,26 @@
+package com.ringtaillemur.rainmaker.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PullRequest {
+
+	@Id
+	@Column(name = "pull_request_id")
+	private Long id;
+	private Long pullRequestNumber;
+	private Long repositoryId;
+	private String pullRequestOpenBranchName;
+	private String pullRequestCloseBranchName;
+	private Boolean leadTimeForChangeProcessEnd;
+	private Long additions;
+	private Long deletions;
+}
