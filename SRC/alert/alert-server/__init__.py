5583816ec82cf525302dd8948a51761347cbd8e8
vidigummy <vidigummy@gamil.com>
2022-11-03 17:17:08 +0900

    ??

--- SRC/alert/alert-server/__init__.py
+++ SRC/alert/alert-server/__init__.py
@@ -1,10 +1,14 @@
-import datetime
-import logging
-import azure.functions as func
-import extractor
-from . import chooser
+
 
 def main(mytimer: func.TimerRequest) -> None:
+    try:
+        import datetime
+        import logging
+        from azure import function as func
+        import extractor
+        from . import chooser
+    except:
+        print('hi')
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
